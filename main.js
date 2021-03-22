dc8d300fa839e123df24b9feb2151f98da8ac6d8
jdeepee <joshuadparkin@gmail.com>
2021-03-22 14:05:36 +0000

    added base files

---
+++ main.js
@@ -0,0 +1,25 @@
+const { app, BrowserWindow } = require('electron');
+const { executor } = require("ad4m-core-executor");
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+    //executor("test");
+    win.loadFile('index.html');
+}
+app.whenReady().then(createWindow);
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
+});
