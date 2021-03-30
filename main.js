c2f9569c4e552854e2f0cddf1110b2b690a3b62a
jdeepee <joshuadparkin@gmail.com>
2021-03-30 12:59:46 +0100

    working ad4m integration

--- main.js
+++ main.js
@@ -1,6 +1,48 @@
+require = require("esm")(module/*, options*/)
+//module.exports = require("./main.js")
 const { app, BrowserWindow } = require('electron');
-const { executor } = require("ad4m-core-executor");
+const ad4m = require("ad4m-core-executor");
 
+app.whenReady().then(() => {
+    console.log("Init AD4M...", ad4m);
+    let ad4mCore = ad4m.init("");
+    console.log("Starting account creation splash screen");
+
+    const splash = createSplash()
+    ad4mCore.waitForAgent().then(() => {
+        console.log("Controllers init");
+        ad4mCore.initControllers();
+        createWindow()
+        splash.close()
+    })
+})
+
+/// This is the window where a user should create their DID
+function createSplash () {
+    // Create the browser window.
+    const win = new BrowserWindow({
+      width: 1000,
+      height: 600,
+      webPreferences: {
+        nodeIntegration: true,
+        enableRemoteModule: true,
+      },
+      minimizable: false,
+      alwaysOnTop: true,
+      frame: false,
+      transparent: true,
+    })
+  
+    // and load the index.html of the app.
+    win.loadFile('index.html');
+  
+    // Open the DevTools.
+    //win.webContents.openDevTools()
+  
+    return win
+}
+  
+/// This is the window which is the main entry point to the application
 function createWindow() {
     const win = new BrowserWindow({
         width: 800,
@@ -9,15 +51,15 @@ function createWindow() {
             nodeIntegration: true
         }
     });
-    //executor("test");
     win.loadFile('index.html');
 }
-app.whenReady().then(createWindow);
+
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
         app.quit();
     }
 });
+
 app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
         createWindow();
