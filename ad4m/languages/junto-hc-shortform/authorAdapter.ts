e3b5895621a4625dc4424e529d6860593f2d329a
jdeepee <joshuadparkin@gmail.com>
2021-03-31 14:44:21 +0100

    added junto short form language with old hc dna

---
+++ ad4m/languages/junto-hc-shortform/authorAdapter.ts
@@ -0,0 +1,33 @@
+import type Expression from "ad4m/Expression";
+import type Address from 'ad4m/Address';
+import Agent from 'ad4m/Agent';
+import type { ExpressionAdapter, GetByAuthorAdapter, PublicSharing } from 'ad4m/Language';
+import type LanguageContext from 'ad4m/LanguageContext';
+import type { default as HolochainLanguageDelegate, HolochainService } from "language-context/lib/Holochain/HolochainLanguageDelegate";
+import { DNA_NICK } from './dna'
+export default class ShortFormAuthorAdapter implements GetByAuthorAdapter {
+    #shortFormDNA: HolochainLanguageDelegate
+    
+    constructor(context: LanguageContext) {
+        this.#shortFormDNA = context.Holochain as HolochainLanguageDelegate
+    }
+
+    //Question: For this author; assuming we resolved with profile DHT; how do we know which agent to use if they have multiple listed?
+    //might not be a clear 1:1 mapping for did to agents
+    ///Get expressions authored by a given Agent/Identity
+    async getByAuthor(author: Agent, count: number, page: number): Promise<void | Expression[]> {
+        //TODO: resolve did
+        let res = await this.#shortFormDNA.call(DNA_NICK, "shortform", "get_by_author", {author: author.did, page_size: count, page_number: page})
+        let out = [];
+        res.forEach(expression => {
+            out.push({
+                //Might need to resolve creator to did information
+                author: new Agent(expression.creator),
+                timestamp: expression.created_at,
+                data: JSON.parse(expression),
+                proof: undefined
+            });
+        });
+        return out
+    }
+}
---
+++ ad4m/languages/junto-hc-shortform/index.ts
@@ -0,0 +1,48 @@
+import type Address from 'ad4m/Address'
+import type Agent from 'ad4m/Agent'
+import type Language from 'ad4m/Language'
+import type LanguageContext from 'ad4m/LanguageContext'
+import type { Interaction } from 'ad4m/Language'
+import ShortFormAdapter from './adapter'
+import ShortFormAuthorAdapter from './authorAdapter'
+import Icon from './build/Icon.js'
+import ConstructorIcon from './build/ConstructorIcon.js'
+import { JuntoSettingsUI } from './SettingsUI'
+import { ShortFormExpressionUI } from './shortFormExpressionUI'
+import { DNA, DNA_NICK } from './dna'
+import type HolochainLanguageDelegate from "language-context/lib/Holochain/HolochainLanguageDelegate"
+
+function iconFor(expression: Address): string {
+    return Icon
+}
+
+function constructorIcon(): string {
+    return ConstructorIcon
+}
+
+function interactions(a: Agent, expression: Address): Interaction[] {
+    return []
+}
+
+export const name: string = "junto-shortform"
+
+export default function create(context: LanguageContext): Language {
+    const Holochain = context.Holochain as HolochainLanguageDelegate
+    Holochain.registerDNAs([{file: DNA, nick: DNA_NICK}])
+
+    const expressionAdapter = new ShortFormAdapter(context)
+    const authorAdaptor = new ShortFormAuthorAdapter(context)
+    const settingsUI = new JuntoSettingsUI()
+    const expressionUI = new ShortFormExpressionUI()
+
+    return {
+        name,
+        expressionAdapter,
+        authorAdaptor,
+        iconFor,
+        constructorIcon,
+        interactions,
+        settingsUI,
+        expressionUI
+    } as Language
+}
