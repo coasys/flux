e7d25ed187233b591686a88ccf140e2a419e21de
jdeepee <joshuadparkin@gmail.com>
2021-04-03 16:21:20 +0100

    added graphql init client & started adding internal language type references & detail createGroup function

---
+++ src/validate.ts
@@ -0,0 +1,6 @@
+import type { JuntoLanguageTypes } from "./junto-langs"
+
+/// Should take an input bundle file, remove var DNA from bundle and then check hash of that bundle vs default installed Junto languages
+export default function validateLanguage(bundle: string, languageType: JuntoLanguageTypes): boolean {
+    return true
+}
