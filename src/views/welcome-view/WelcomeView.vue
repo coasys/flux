9f912ea5fcc21940a4700ead680d4e83ad93eb67
jdeepee <joshuadparkin@gmail.com>
2021-04-09 18:21:42 +0100

    hookup first graphql query!

--- src/views/welcome-view/WelcomeView.vue
+++ src/views/welcome-view/WelcomeView.vue
@@ -8,13 +8,36 @@
   </div>
 </template>
 
-<script>
+<script lang="ts">
+import { defineComponent } from "vue";
+import { AGENT_SERVICE_STATUS } from "../../core/graphql_queries";
+import { useQuery, useResult, useMutation } from "@vue/apollo-composable";
+import ad4m from "ad4m-core-executor";
+
 import WelcomeViewHeader from "./components/WelcomeViewHeader.vue";
 import WelcomeViewLeft from "./components/WelcomeViewLeft.vue";
 import WelcomeViewRight from "./components/WelcomeViewRight.vue";
-export default {
+
+export default defineComponent({
+  name: "Welcome",
   components: { WelcomeViewHeader, WelcomeViewLeft, WelcomeViewRight },
-};
+  mounted() {
+    window.api.send("ping");
+    window.api.receive("pong", (data: any) => {
+      console.log(`Received pong: ${data}`);
+    });
+
+    const { onResult } = useQuery<{ agent: ad4m.AgentService }>(
+      AGENT_SERVICE_STATUS
+    );
+    onResult((queryResult) => {
+      console.log(queryResult.data);
+      console.log("init", queryResult.data.agent.isInitialized);
+      console.log(queryResult.loading);
+      console.log(queryResult.networkStatus);
+    });
+  },
+});
 </script>
 
 <style lang="scss" scoped>
