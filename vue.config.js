1283eb6e6f05f2825656d047b39d610dd7f6c5cf
jdeepee <joshuadparkin@gmail.com>
2021-04-13 14:39:21 +0100

    fix production building

--- vue.config.js
+++ vue.config.js
@@ -1,27 +1,23 @@
 module.exports = {
   pluginOptions: {
     electronBuilder: {
-      chainWebpackMainProcess: (config) => {
-        // Chain webpack config for electron main process only
-        //config.module.rule("typescript").use("babel-loader").loader("babel-loader");
-        // config.module
-        //   .rule("core-typescript")
-        //   .use("ts-loader")
-        //   .loader("ts-loader")
-        //   .options({
-        //     configFile: __dirname + "/core.tsconfig.json",
-        //   });
-        // config
-        //   .entry("core")
-        //   .add(__dirname + "/src/core/create.ts")
-        //   .end()
-        //   // Modify output settings
-        //   .output.path(__dirname + "/core_dist")
-        //   .filename("[name].bundle.js");
-        // return config;
-      },
       preload: "src/preload.js",
       externals: ["ad4m-core-executor"],
+      builderOptions: {
+        productName: "Communities",
+        extraResources: [
+          {
+            from: "resources/${os}",
+            to: "Resources/bin",
+            filter: ["**/*"],
+          },
+          {
+            from: "ad4m/languages",
+            to: "Resources/languages",
+            filter: ["**/*"],
+          },
+        ],
+      },
     },
   },
 };
