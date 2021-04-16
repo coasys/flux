c9f32c48f209b63c3b9d13bf8edc37109e26ef3b
jdeepee <joshuadparkin@gmail.com>
2021-04-16 16:17:42 +0100

    revert shared-perspective to ipfs implementation since its more ad4m

--- ad4m/languages/shared-perspectives/adapter.ts
+++ ad4m/languages/shared-perspectives/adapter.ts
@@ -1,62 +1,98 @@
-import type Address from 'ad4m/Address'
-import type Expression from 'ad4m/Expression'
-import type { ExpressionAdapter, PublicSharing } from 'ad4m/Language'
-import type LanguageContext from 'ad4m/LanguageContext'
-import type { default as HolochainLanguageDelegate } from "language-context/lib/Holochain/HolochainLanguageDelegate"
-import type AgentService from "ad4m/AgentService"
-import { DNA_NICK } from './dna'
+import type Address from "ad4m/Address";
+import type Expression from "ad4m/Expression";
+import type { ExpressionAdapter, PublicSharing } from "ad4m/Language";
+import type LanguageContext from "ad4m/LanguageContext";
+//import type { default as HolochainLanguageDelegate } from "language-context/lib/Holochain/HolochainLanguageDelegate";
+import type AgentService from "ad4m/AgentService";
+//import { DNA_NICK } from "./dna";
+import type { IPFSNode } from "ad4m/LanguageContext";
 
 const _appendBuffer = (buffer1, buffer2) => {
-    const tmp = new Uint8Array(buffer1.byteLength + buffer2.byteLength);
-    tmp.set(new Uint8Array(buffer1), 0);
-    tmp.set(new Uint8Array(buffer2), buffer1.byteLength);
-    return tmp.buffer;
+  const tmp = new Uint8Array(buffer1.byteLength + buffer2.byteLength);
+  tmp.set(new Uint8Array(buffer1), 0);
+  tmp.set(new Uint8Array(buffer2), buffer1.byteLength);
+  return tmp.buffer;
 };
 
 const uint8ArrayConcat = (chunks) => {
-    return chunks.reduce(_appendBuffer)
-}
+  return chunks.reduce(_appendBuffer);
+};
 
 class SharedPerspectivePutAdapter implements PublicSharing {
-    #agent: AgentService
-    #hcDna: HolochainLanguageDelegate
+  #agent: AgentService;
+  #IPFS: IPFSNode;
+  //#hcDna: HolochainLanguageDelegate;
 
-    constructor(context: LanguageContext) {
-        this.#agent = context.agent
-        this.#hcDna = context.Holochain as HolochainLanguageDelegate
-    }
+  constructor(context: LanguageContext) {
+    this.#agent = context.agent;
+    this.#IPFS = context.IPFS;
+    //this.#hcDna = context.Holochain as HolochainLanguageDelegate;
+  }
 
-    async createPublic(shortForm: object): Promise<Address> {
-        //@ts-ignore
-        let obj = JSON.parse(shortForm)
-        const expression = this.#agent.createSignedExpression(obj.sharedPerspective)
-        const reqData = {key: obj.key, sharedPerspective: expression}
+  async createPublic(sharedPerspective: object): Promise<Address> {
+    // console.log("Got object", sharedPerspective);
+    // //@ts-ignore
+    // const obj = JSON.parse(sharedPerspective);
+    // console.log("parsed into", obj);
+    // const expression = this.#agent.createSignedExpression(sharedPerspective);
+    // console.log("Signed expression", expression);
+    // const reqData = { key: obj.key, sharedPerspective: expression };
+    // console.log("sending req", reqData);
 
-        await this.#hcDna.call(DNA_NICK, "shared_perspective_index", "index_shared_perspective", reqData);
-        return reqData.key;
-    }
+    // await this.#hcDna.call(
+    //   DNA_NICK,
+    //   "shared_perspective_index",
+    //   "index_shared_perspective",
+    //   reqData
+    // );
+    // return reqData.key;
+    const agent = this.#agent;
+    const expression = agent.createSignedExpression(sharedPerspective);
+    const content = JSON.stringify(expression);
+    const result = await this.#IPFS.add({ content });
+    // @ts-ignore
+    return result.cid.toString() as Address;
+  }
 }
 
 export default class Adapter implements ExpressionAdapter {
-    #hcDna: HolochainLanguageDelegate
+  #IPFS: IPFSNode;
+  //#hcDna: HolochainLanguageDelegate;
 
-    putAdapter: PublicSharing
+  putAdapter: PublicSharing;
 
-    constructor(context: LanguageContext) {
-        this.#hcDna = context.Holochain as HolochainLanguageDelegate
-        this.putAdapter = new SharedPerspectivePutAdapter(context)
+  constructor(context: LanguageContext) {
+    this.#IPFS = context.IPFS;
+    //this.#hcDna = context.Holochain as HolochainLanguageDelegate;
+    this.putAdapter = new SharedPerspectivePutAdapter(context);
+  }
+
+  async get(address: Address): Promise<void | Expression> {
+    const cid = address.toString();
+
+    const chunks = [];
+    // @ts-ignore
+    for await (const chunk of this.#IPFS.cat(cid)) {
+      chunks.push(chunk);
     }
 
-    async get(address: Address): Promise<void | Expression> {
-        const cid = address.toString()
-        //TODO: right now we are just returning the latest shared perspective under a given index but we actually will want to return
-        //all sharedperspectives. This might mean changing the way expression signing works or just ignoring expression signing for the interim.
-        let res = await this.#hcDna.call(DNA_NICK, "shared_perspective_index", "get_latest_shared_perspective", cid);
-        if (res != null) {
-            let expr: Expression = Object.assign(res.expression_data)
-            return expr
-        } else {
-            return null
-        }
-    }
+    const fileString = uint8ArrayConcat(chunks).toString();
+    const fileJson = JSON.parse(fileString);
+    return fileJson;
+    // const cid = address.toString();
+    // //TODO: right now we are just returning the latest shared perspective under a given index but we actually will want to return
+    // //all sharedperspectives. This might mean changing the way expression signing works or just ignoring expression signing for the interim.
+    // const res = await this.#hcDna.call(
+    //   DNA_NICK,
+    //   "shared_perspective_index",
+    //   "get_latest_shared_perspective",
+    //   cid
+    // );
+    // if (res != null) {
+    //   const expr: Expression = Object.assign(res.expression_data);
+    //   return expr;
+    // } else {
+    //   return null;
+    // }
+  }
 }
--- ad4m/languages/shared-perspectives/index.ts
+++ ad4m/languages/shared-perspectives/index.ts
@@ -1,36 +1,35 @@
-import type Address from 'ad4m/Address'
-import type Agent from 'ad4m/Agent'
-import type Language from 'ad4m/Language'
-import type LanguageContext from 'ad4m/LanguageContext'
-import type { Interaction } from 'ad4m/Language'
-import Adapter from './adapter'
+import type Address from "ad4m/Address";
+import type Agent from "ad4m/Agent";
+import type Language from "ad4m/Language";
+import type LanguageContext from "ad4m/LanguageContext";
+import type { Interaction } from "ad4m/Language";
+import Adapter from "./adapter";
 import type { ExpressionUI as Interface } from "ad4m/Language";
 
 function interactions(a: Agent, expression: Address): Interaction[] {
-    return []
+  return [];
 }
 
 export class ExpressionUI implements Interface {
-    icon(): string {
-        return ""
-    }
+  icon(): string {
+    return "";
+  }
 
-    constructorIcon(): string {
-        return ""
-    }
+  constructorIcon(): string {
+    return "";
+  }
 }
 
-export const name: string = "shared-perspectives"
+export const name = "shared-perspectives";
 
 export default function create(context: LanguageContext): Language {
-    const expressionAdapter = new Adapter(context)
-    const expressionUI = new ExpressionUI()
+  const expressionAdapter = new Adapter(context);
+  const expressionUI = new ExpressionUI();
 
-    return {
-        name,
-        expressionAdapter,
-        expressionUI,
-        interactions,
-    } as Language
+  return {
+    name,
+    expressionAdapter,
+    expressionUI,
+    interactions,
+  } as Language;
 }
-
