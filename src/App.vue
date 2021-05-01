1c922fc43e0ef9067a717551552d570f03cd1dda
jdeepee <joshuadparkin@gmail.com>
2021-05-01 18:22:50 +0100

    add logic for signal store pushing & remove un-used file

--- src/App.vue
+++ src/App.vue
@@ -6,13 +6,37 @@
 import { useSubscription } from "@vue/apollo-composable";
 import { defineComponent, watch } from "vue";
 import { AD4M_SIGNAL } from "./core/graphql_queries";
+import { useStore } from "vuex";
+import Expression from "@perspect3vism/ad4m/Expression";
+
+declare global {
+  interface Window {
+    api: any;
+  }
+}
 
 export default defineComponent({
   name: "App",
   setup() {
+    const store = useStore();
     const { result } = useSubscription(AD4M_SIGNAL);
+
     watch(result, (data) => {
-      console.log("\n\nSIGNAL RECEIVED IN UI:", data);
+      console.log("\n\nSIGNAL RECEIVED IN UI:", JSON.parse(data.signal.signal));
+      console.log("Coming from language", data.signal.language);
+      let expression = data.signal.signal.data.payload;
+      console.log("Parse as exp", expression);
+      if (Object.prototype.hasOwnProperty.call(expression.data, "source") 
+        && Object.prototype.hasOwnProperty.call(expression.data, "target") 
+          && Object.prototype.hasOwnProperty.call(expression.data, "predicate")) {
+        //@ts-ignore
+        if (expression.data.predicate == "sioc://content_of") {
+          store.commit({
+            type: "pushLinkExpression",
+            value: [data.signal.language, expression],
+          });
+        }
+      }
     });
 
     return {};
