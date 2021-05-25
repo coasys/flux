9b63d05dd41f349a611a707090194d6de6312667
Joshua Parkin <joshuadparkin@gmail.com>
2021-05-25 22:36:49 +0100

    update clean state command to use correct path for mac and linux

--- clean-state.js
+++ clean-state.js
@@ -1,25 +1,53 @@
 const path = require("path");
+const fs = require("fs");
+const readline = require('readline');
 
-let configPath;
+function askQuestion(query) {
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
 
-switch (process.platform) {
-  case "darwin": {
-    configPath = path.join(
-      process.env.HOME,
-      "Library",
-      "Application Support",
-      "ad4m"
-    );
-    break;
-  }
-  case "win32": {
-    configPath = path.join(process.env.APPDATA, "ad4m");
-    break;
-  }
-  case "linux": {
-    configPath = path.join(process.env.HOME, ".config", "ad4m");
-    break;
-  }
+    return new Promise((resolve, reject) => rl.question(query, ans => {
+        if (ans == "y" || ans == "Y") {
+          rl.close();
+          resolve(ans);
+        } else {
+          console.log("\n Aborting...");
+          process.exit(1);
+        }
+    }))
 }
 
-console.log("Trying to delete", configPath);
+async function main() {
+  let configPath;
+
+  switch (process.platform) {
+    case "darwin": {
+      configPath = path.join(
+        process.env.HOME,
+        "Library",
+        "Application Support",
+        "ad4m"
+      );
+      break;
+    }
+    case "win32": {
+      configPath = path.join(process.env.APPDATA, "ad4m");
+      break;
+    }
+    case "linux": {
+      configPath = path.join(process.env.HOME, ".config", "ad4m");
+      break;
+    }
+  }
+  
+  await askQuestion(`Deleting ${configPath} config path.. Press Y to accept... `);
+  fs.rmdirSync(configPath, { recursive: true });
+  if (fs.existsSync("./hc")) {
+    fs.rmSync("./hc");
+  }
+  console.log("Done. Please run localStorage.clear() in the browser to clear Vuex store.")
+}
+
+main()
