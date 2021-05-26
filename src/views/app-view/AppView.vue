fdbe4e17e0015416332faf182cdadb1f996885b6
leifriksheim <riksheim.leif@gmail.com>
2021-05-26 23:30:31 +0200

    check if we're logged in, and redirect if not

--- src/views/app-view/AppView.vue
+++ src/views/app-view/AppView.vue
@@ -12,51 +12,40 @@ import CommunityView from "./../community-view/CommunityView.vue";
 import { defineComponent } from "vue";
 import { PERSPECTIVES } from "../../core/graphql_queries";
 import { useQuery } from "@vue/apollo-composable";
+import { AGENT_SERVICE_STATUS } from "../../core/graphql_queries";
+import ad4m from "@perspect3vism/ad4m-executor";
+import { databasePerspectiveName } from "../../core/juntoTypes";
 
 export default defineComponent({
   name: "MainAppView",
-  // setup() {
-  //   const {
-  //     onResult: getPerspectives,
-  //     onError: getPerspectivesError,
-  //   } = useQuery<{
-  //     perspectives: [ad4m.Perspective];
-  //   }>(PERSPECTIVES);
-
-  //   return {
-  //     getPerspectives,
-  //     getPerspectivesError,
-  //   };
-  // },
-  // mounted() {
-  //   this.getPerspectives((perspectives) => {
-  //     console.log("Found perspectives", perspectives);
-  //     if (perspectives.data.perspectives.length > 0) {
-  //       for (var i = 0; i < perspectives.data.perspectives.length; i++) {
-  //         if (
-  //           this.$store.getters.getCommunityById(
-  //             perspectives.data.perspectives[i].uuid
-  //           ) == undefined
-  //         ) {
-  //           //NOTE: this logic is actually incorrect; there is no mapping between perspective -> community
-  //           //since a community can contain more than one perspective (one for each channel)
-  //           //There is also the possiblity that we add a database perspective in the future and of course this will also not be a community
-  //           this.$store.commit({
-  //             type: "addCommunity",
-  //             value: {
-  //               name: perspectives.data.perspectives[i].name,
-  //               channels: [],
-  //               perspective: perspectives.data.perspectives[i].uuid,
-  //               expressionLanguages:
-  //                 perspectives.data.perspectives[i].sharedPerspective
-  //                   ?.requiredExpressionLanguages,
-  //             },
-  //           });
-  //         }
-  //       }
-  //     }
-  //   });
-  // },
+  data() {
+    return {
+      isInit: false,
+    };
+  },
+  beforeCreate() {
+    const { onResult, onError } =
+      useQuery<{
+        agent: ad4m.AgentService;
+      }>(AGENT_SERVICE_STATUS);
+    onResult((val) => {
+      this.isInit = val.data.agent.isInitialized!;
+      this.$store.commit({ type: "updateAgentLockState", value: false });
+      if (this.isInit) {
+        //Get database perspective from store
+        let databasePerspective = this.$store.getters.getDatabasePerspective;
+        if (databasePerspective == "") {
+          console.warn(
+            "Does not have databasePerspective in store but has already been init'd! Add logic for getting databasePerspective as found with name",
+            databasePerspectiveName
+          );
+          //TODO: add the retrieval/state saving logic here
+        }
+      } else {
+        this.$router.push("/signup");
+      }
+    });
+  },
   components: {
     LeftNav,
     CommunityView,
