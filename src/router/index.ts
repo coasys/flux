5f3f85a68f1e67cd3a6a88dbcd32516c11f354f5
leifriksheim <riksheim.leif@gmail.com>
2021-05-26 16:08:06 +0200

    feature: start on routing

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,17 +1,33 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 import AppView from "./../views/app-view/AppView.vue";
 import WelcomeView from "./../views/welcome-view/WelcomeView.vue";
+import CommunityView from "./../views/community-view/CommunityView.vue";
+import MainView from "./../views/community-view/main-view/MainView.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
-    path: "/",
-    name: "welcome view",
+    path: "/signup",
+    name: "signup",
     component: WelcomeView,
   },
   {
-    path: "/home",
+    path: "/",
     name: "home",
     component: AppView,
+    children: [
+      {
+        path: "communities/:communityId",
+        name: "community",
+        component: CommunityView,
+        children: [
+          {
+            path: ":perspectiveId",
+            name: "perspective",
+            component: MainView,
+          },
+        ],
+      },
+    ],
   },
 ];
 
