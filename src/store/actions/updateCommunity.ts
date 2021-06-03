ec47a0b7fbaff6f9b9e05ffb81b127be4f8523dd
leifriksheim <riksheim.leif@gmail.com>
2021-06-03 15:38:28 +0200

    fix: use a getter to get community by id instead of passing the whole object

--- src/store/actions/updateCommunity.ts
+++ src/store/actions/updateCommunity.ts
@@ -1,29 +1,42 @@
-import { Commit, Store } from "vuex";
-import { CommunityState, JuntoExpressionReference, ExpressionTypes } from "..";
-import {createExpression} from "@/core/mutations/createExpression";
-import {createLink} from "@/core/mutations/createLink";
+import { Commit } from "vuex";
+import { JuntoExpressionReference, ExpressionTypes } from "..";
+import { createExpression } from "@/core/mutations/createExpression";
+import { createLink } from "@/core/mutations/createLink";
 
 export interface Context {
-    commit: Commit;
-    getters: any;
-}
-
-export interface GroupExpressionData {
-    name: string,
-    description: string,
+  commit: Commit;
+  getters: any;
 }
 
 export interface Payload {
-    community: CommunityState,
-    groupExpressionData: GroupExpressionData
+  communityId: string;
+  name: string;
+  description: string;
 }
 
-export default async function updateCommunity({commit, getters}: Context, {community, groupExpressionData}: Payload) {
-    const groupExpressionLang = community.typedExpressionLanguages.find((val: JuntoExpressionReference) => val.expressionType == ExpressionTypes.GroupExpression);
+export default async function updateCommunity(
+  { commit, getters }: Context,
+  { communityId, name, description }: Payload
+): Promise<void> {
+  const community = getters.getCommunity(communityId);
+
+  try {
+    const groupExpressionLang = community.typedExpressionLanguages.find(
+      (val: JuntoExpressionReference) =>
+        val.expressionType == ExpressionTypes.GroupExpression
+    );
+
     if (groupExpressionLang != undefined) {
       console.log("Found group exp lang", groupExpressionLang);
-      const groupExpression = await createExpression(groupExpressionLang.languageAddress, JSON.stringify(groupExpressionData));
-      console.log("Created new group expression for updateCommunity", groupExpression);
+      const groupExpression = await createExpression(
+        groupExpressionLang.languageAddress,
+        JSON.stringify({ name, description })
+      );
+
+      console.log(
+        "Created new group expression for updateCommunity",
+        groupExpression
+      );
 
       const addGroupExpLink = await createLink(community.perspective, {
         source: `${community.linkLanguageAddress}://self`,
@@ -32,14 +45,16 @@ export default async function updateCommunity({commit, getters}: Context, {commu
       });
       console.log("Created group expression link", addGroupExpLink);
 
-      commit("updateCommunityMetadata", 
-        {
-          communityId: community.perspective, 
-          name: groupExpressionData.name, 
-          description: groupExpressionData.description, 
-          groupExpressionRef: groupExpression
-      })
+      commit("updateCommunityMetadata", {
+        communityId: community.perspective,
+        name: name,
+        description: description,
+        groupExpressionRef: groupExpression,
+      });
     } else {
-      throw Error("Expected to find group expression language for group")
+      throw Error("Expected to find group expression language for group");
     }
+  } catch (e) {
+    throw new Error(e);
+  }
 }
