af9b874bdbc74f1692c044e592f31000fe07c33d
leifriksheim <riksheim.leif@gmail.com>
2021-06-04 12:19:58 +0200

    chore: rename component to avatar-upload

--- src/views/welcome-view/WelcomeView.vue
+++ src/views/welcome-view/WelcomeView.vue
@@ -38,13 +38,11 @@
       <j-flex direction="column" gap="400" v-else>
         <j-text variant="heading">Sign up</j-text>
         <div class="welcome-view--center">
-          <profile-avatar
-            :diameter="10"
-            :enableFileSelection="true"
-            :onClick="selectFile"
-            :profileImage="profileImage"
+          <avatar-upload
+            :value="profileImage"
+            @change="(url) => (profileImage = url)"
           >
-          </profile-avatar>
+          </avatar-upload>
         </div>
         <j-input
           label="First Name"
@@ -80,22 +78,6 @@
       </j-flex>
     </div>
   </div>
-
-  <div class="cropper_parent" v-if="tempProfileImage !== null">
-    <cropper
-      ref="cropper"
-      class="cropper"
-      backgroundClass="cropper__background"
-      :src="tempProfileImage"
-      :stencil-props="{
-        aspectRatio: 12 / 12,
-      }"
-    ></cropper>
-    <div class="cropper_parent__btns">
-      <button class="cropper_parent__btn" @click="clearImage">cancel</button>
-      <button class="cropper_parent__btn" @click="selectImage">submit</button>
-    </div>
-  </div>
 </template>
 
 <script lang="ts">
@@ -110,8 +92,7 @@ import {
 import { useQuery, useMutation } from "@vue/apollo-composable";
 import ad4m from "@perspect3vism/ad4m-executor";
 import { databasePerspectiveName } from "../../core/juntoTypes";
-import ProfileAvatar from "@/components/ui/avatar/ProfileAvatar.vue";
-import { Cropper } from "vue-advanced-cropper";
+import AvatarUpload from "@/components/avatar-upload/AvatarUpload.vue";
 import "vue-advanced-cropper/dist/style.css";
 import {
   blobToDataURL,
@@ -151,11 +132,9 @@ export default defineComponent({
   },
   data(): {
     profileImage: string | ArrayBuffer | null | undefined;
-    tempProfileImage: any;
   } {
     return {
       profileImage: null,
-      tempProfileImage: null,
     };
   },
   methods: {
@@ -189,30 +168,9 @@ export default defineComponent({
           this.logInError = true;
         });
     },
-    selectFile(e: any) {
-      const files = e.target.files || e.dataTransfer.files;
-      if (!files.length) return;
-
-      var reader = new FileReader();
-
-      reader.onload = (e) => {
-        this.tempProfileImage = e.target?.result;
-      };
-
-      reader.readAsDataURL(files[0]);
-    },
-    clearImage() {
-      this.tempProfileImage = null;
-    },
-    selectImage() {
-      const result = (this.$refs.cropper as any).getResult();
-      this.profileImage = result.canvas.toDataURL();
-      this.tempProfileImage = null;
-    },
   },
   components: {
-    ProfileAvatar,
-    Cropper,
+    AvatarUpload,
   },
 });
 </script>
@@ -250,42 +208,5 @@ export default defineComponent({
 
 .welcome-view__logo {
   margin-bottom: var(--j-space-600);
-}
-
-.cropper_parent {
-  position: fixed;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  width: 800px;
-  height: 600px;
-  background: black;
-  display: flex;
-  flex-direction: column;
-  border-radius: 4px;
-
-  &__btns {
-    display: flex;
-    width: 100%;
-  }
-
-  &__btn {
-    height: 40px;
-    width: 50%;
-    font-size: 18px;
-    background: white;
-
-    &:hover {
-      background: rgb(235, 235, 235);
-      transition: all 0.2s;
-    }
-  }
-}
-
-.cropper {
-  flex-grow: 1;
-  &__background {
-    background: transparent !important;
-  }
 }
 </style>
