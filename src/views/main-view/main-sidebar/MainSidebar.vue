625147af7b11e6e6d94321fb8e787d37c2e12c7b
leifriksheim <riksheim.leif@gmail.com>
2021-06-07 12:02:54 +0200

    cleanup: remove setup function, doesn't do anything at the moment

--- src/views/main-view/main-sidebar/MainSidebar.vue
+++ src/views/main-view/main-sidebar/MainSidebar.vue
@@ -1,88 +1,22 @@
 <template>
-  <div class="left-nav">
+  <div class="sidebar-container">
     <communities-list></communities-list>
     <bottom-section></bottom-section>
   </div>
 </template>
 
 <script lang="ts">
+import { defineComponent } from "vue";
 import CommunitiesList from "./CommunitiesList.vue";
 import BottomSection from "./BottomSection.vue";
 
-import { useStore } from "vuex";
-import { defineComponent, ref } from "vue";
-import { CommunityState } from "@/store";
-
 export default defineComponent({
   components: { CommunitiesList, BottomSection },
-  setup() {
-    const store = useStore();
-
-    const joiningLink = ref("");
-
-    const newCommunityName = ref("");
-    const newCommunityDesc = ref("");
-
-    const showModal = ref(false);
-    const isCreatingCommunity = ref(false);
-    const tabView = ref("Create");
-
-    const changeCommunity = (community: CommunityState) => {
-      store.commit("changeCommunity", community);
-    };
-
-    const handleCommunityClick = (community: CommunityState) => {
-      changeCommunity(community);
-    };
-
-    const createCommunity = () => {
-      isCreatingCommunity.value = true;
-      store
-        .dispatch("createCommunity", {
-          perspectiveName: newCommunityName.value,
-          description: newCommunityDesc.value,
-        })
-        .then(() => {
-          showModal.value = false;
-          newCommunityName.value = "";
-          newCommunityDesc.value = "";
-        })
-        .finally(() => {
-          isCreatingCommunity.value = false;
-        });
-    };
-
-    const joinCommunity = () => {
-      store.dispatch("joinCommunity", {
-        joiningLink: joiningLink.value,
-      });
-    };
-
-    return {
-      joiningLink,
-      joinCommunity,
-      newCommunityName,
-      newCommunityDesc,
-      createCommunity,
-      tabView,
-      showModal,
-      isCreatingCommunity,
-      handleCommunityClick,
-    };
-  },
-
-  computed: {
-    getCommunities() {
-      const communities = this.$store.getters.getCommunities;
-
-      return communities;
-    },
-  },
 });
 </script>
 
-<style lang="scss">
-.left-nav {
+<style scoped>
+.sidebar-container {
   padding-top: var(--j-space-400);
   height: 100vh;
   width: 100%;
