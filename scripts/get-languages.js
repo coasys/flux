edbbe274c83d62e33b06b16711dd995a6f9e2ade
jdeepee <joshuadparkin@gmail.com>
2021-06-16 11:14:49 +0100

    use script to get released profile language

---
+++ scripts/get-languages.js
@@ -0,0 +1,34 @@
+const fs = require("fs-extra");
+const wget = require("node-wget-js");
+
+const languages = {
+  profiles: {
+    targetDnaName: "agent-profiles",
+    dna: "https://github.com/jdeepee/profiles/releases/download/0.0.1/agent-profiles.dna",
+    bundle:
+      "https://github.com/jdeepee/profiles/releases/download/0.0.1/bundle.js",
+  },
+};
+
+async function main() {
+  for (const lang in languages) {
+    const dir = `./ad4m/languages/${lang}`;
+    await fs.ensureDir(dir + "/build");
+
+    // bundle
+    if (languages[lang].bundle) {
+      let url = languages[lang].bundle;
+      let dest = dir + "/build/bundle.js";
+      wget({ url, dest });
+    }
+
+    // dna
+    if (languages[lang].dna) {
+      url = languages[lang].dna;
+      dest = dir + `/${languages[lang].targetDnaName}.dna`;
+      wget({ url, dest });
+    }
+  }
+}
+
+main();
