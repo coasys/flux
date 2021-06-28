639e5e0a20f3867a635a7435f13e76ebd7e48d9a
Leif Riksheim <riksheim.leif@gmail.com>
2021-06-28 20:01:27 +0200

    wip: new expression lang

---
+++ ad4m/languages/youtube/index.ts
@@ -0,0 +1,34 @@
+import type LanguageContext from "@perspect3vism/ad4m/LanguageContext";
+import type Language from "@perspect3vism/ad4m/Language";
+import type { Interaction } from "@perspect3vism/ad4m/Language";
+import ConstructorIcon from "./constructor-icon";
+
+export const name = "junto-youtube";
+
+export interface Content {
+  url: string;
+}
+
+function PutAdapter() {
+  this.addressOf = async (content) => content.url;
+}
+
+function ExpressionAdapter() {
+  this.putAdapter = new PutAdapter();
+  this.get = () => {};
+}
+
+function interactions(): Interaction[] {
+  return [];
+}
+
+export default async function create(
+  context: LanguageContext
+): Promise<Language> {
+  return {
+    name,
+    expressionAdapter: new ExpressionAdapter(),
+    constructorIcon: ConstructorIcon,
+    interactions,
+  } as Language;
+}
