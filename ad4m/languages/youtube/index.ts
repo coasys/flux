84702a46465291bfc8e497f00ff2c040de8aed93
Leif Riksheim <riksheim.leif@gmail.com>
2021-06-29 12:06:54 +0200

    wip: use dynamic import to get string

--- ad4m/languages/youtube/index.ts
+++ ad4m/languages/youtube/index.ts
@@ -1,7 +1,7 @@
 import type LanguageContext from "@perspect3vism/ad4m/LanguageContext";
 import type Language from "@perspect3vism/ad4m/Language";
 import type { Interaction } from "@perspect3vism/ad4m/Language";
-import ConstructorIcon from "./constructor-icon";
+import ConstructorIcon from "./constructor-icon.js";
 
 export const name = "junto-youtube";
 
@@ -28,7 +28,14 @@ export default async function create(
   return {
     name,
     expressionAdapter: new ExpressionAdapter(),
-    constructorIcon: ConstructorIcon,
+    expressionUI: {
+      constructorIcon() {
+        return ConstructorIcon as string;
+      },
+      icon() {
+        return ConstructorIcon as string;
+      },
+    },
     interactions,
   } as Language;
 }
