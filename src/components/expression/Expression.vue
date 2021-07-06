4ca556f5eedf81d0f500ddcbea8136b120598238
Leif Riksheim <riksheim.leif@gmail.com>
2021-07-06 15:03:17 +0200

    chore: vue component for each expression

---
+++ src/components/expression/Expression.vue
@@ -0,0 +1,23 @@
+<template>
+  <div ref="container"></div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  props: {
+    element: {
+      type: String,
+      required: true,
+    },
+    expression: Object,
+  },
+  mounted() {
+    const element = document.createElement(this.element) as any;
+    element.expression = this.expression;
+    // @ts-ignore
+    this.$refs.container.appendChild(element);
+  },
+});
+</script>
