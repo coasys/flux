39ce58dc18aaea614fc1803ec0584b1099f8d2bf
Leif Riksheim <riksheim.leif@gmail.com>
2021-07-06 16:01:20 +0200

    add typescript types

--- ad4m/languages/junto-youtube/index.ts
+++ ad4m/languages/junto-youtube/index.ts
@@ -1,18 +1,21 @@
 import type Language from "@perspect3vism/ad4m/Language";
+import type LanguageContext from "@perspect3vism/ad4m/LanguageContext";
 
 // @ts-ignore
 import ConstructorIcon from "inline:./icons/constructor-icon.js";
 // @ts-ignore
 import ExpressionIcon from "inline:./icons/expression-icon.js";
 
-export default async function create(context) {
+export default async function create(
+  context: LanguageContext
+): Promise<Language> {
   return {
     name: "junto-youtube",
     expressionAdapter: {
       putAdapter: {
-        addressOf: (content: any) => content.url,
+        addressOf: async (content: any) => content.url,
       },
-      get() {
+      async get() {
         return null;
       },
     },
@@ -30,5 +33,5 @@ export default async function create(context) {
       },
     },
     interactions: () => [],
-  } as Language;
+  };
 }
