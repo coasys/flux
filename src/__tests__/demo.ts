ff412070899f8ad48cf77d14b6e893338d70a3a9
fayeed <fayeed@live.com>
2021-07-09 12:54:08 +0530

    test(mutation): Added test for addCommunity & addChannel mutations

--- src/__tests__/demo.ts
+++
@@ -1,9 +0,0 @@
-import '@testing-library/vue';
-import Store from '../store';
-
-describe('Demo test', () => {  
-  test('Demo test - 1', async () => {
-    const store = Store;
-    console.log("store.state", store.state);
-  });
-});
