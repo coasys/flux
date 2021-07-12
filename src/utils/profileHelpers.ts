b2d4cf297f3c49616611e79d883fffc325dd5082
fayeed <fayeed@live.com>
2021-07-12 15:41:24 +0530

    test(utils): Added test for profileHelper removed toProfile function

--- src/utils/profileHelpers.ts
+++ src/utils/profileHelpers.ts
@@ -3,25 +3,6 @@ import { Profile } from "@/store";
 import type Expression from "@perspect3vism/ad4m/Expression";
 import { TimeoutCache } from "./timeoutCache";
 
-export function toProfile(obj: { [x: string]: any }): Profile {
-  const profile: Profile = {
-    username: obj["foaf:AccountName"],
-    email: obj["schema:email"],
-    familyName: obj["schema:familyName"],
-    givenName: obj["schema:givenName"],
-    thumbnailPicture: undefined,
-    profilePicture: undefined,
-  };
-
-  if (obj["schema:image"]) {
-    profile.profilePicture = obj["schema:image"]["schema:contentUrl"];
-    profile.thumbnailPicture =
-      obj["schema:image"]["schema:thumbnail"]["schema:contentUrl"];
-  }
-
-  return profile;
-}
-
 export async function getProfile(
   profileLangAddress: string,
   did: string
