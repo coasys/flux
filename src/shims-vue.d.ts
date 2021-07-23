eee25c6a9daf63fa2aec9e2b4b806edf5d155026
Leif Riksheim <riksheim.leif@gmail.com>
2021-07-23 17:45:23 +0200

    remove shim stuff, no idea what happened

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,20 +1,9 @@
 /* eslint-disable */
 
-import Vue from "vue";
-import VueRouter from "vue-router";
-import { Route } from "vue-router";
-
 declare module "*.vue" {
   import type { DefineComponent } from "vue";
   const component: DefineComponent<{}, {}, any>;
   export default component;
-}
-
-declare module "vue/types/vue" {
-  interface Vue {
-    $router: VueRouter;
-    $route: Route;
-  }
 }
 
 declare module "vue3-virtual-scroller";
