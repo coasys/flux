9241d0265ebc78ba4ae2a412db148fe4860002dc
Leif Riksheim <riksheim.leif@gmail.com>
2021-08-17 17:28:11 +0200

    chore: change to ghost button

--- src/views/signup/SignUp.vue
+++ src/views/signup/SignUp.vue
@@ -40,7 +40,7 @@
           <j-button
             @keydown.stop
             @click.stop="showPassword = !showPassword"
-            variant="transparent"
+            variant="ghost"
             square
             slot="end"
           >
