b19cf26b697e4763155145254f50039be2a856e7
Leif Riksheim <riksheim.leif@gmail.com>
2021-08-18 22:23:22 +0200

    util: add scrollHandler utility function that takes callbacks

--- src/utils/scroll.ts
+++ src/utils/scroll.ts
@@ -4,3 +4,31 @@ export function isAtBottom(scrollContainer: HTMLElement): boolean {
     scrollContainer.scrollTop
   );
 }
+
+export interface ScrollHandlerOptions {
+  onScroll: any;
+  onScrollStop: any;
+  removeListenerAfterStop?: boolean;
+}
+
+export function scrollHandler(
+  scrollContainer: HTMLElement,
+  options: ScrollHandlerOptions
+): void {
+  let _timer = null as null | ReturnType<typeof setTimeout>;
+
+  function handleScroll(e: Event) {
+    options.onScroll(e);
+    if (_timer) {
+      clearTimeout(_timer);
+    }
+    _timer = setTimeout(() => {
+      options.onScrollStop(e);
+      if (options.removeListenerAfterStop) {
+        scrollContainer.removeEventListener("scroll", handleScroll);
+      }
+    }, 150);
+  }
+
+  scrollContainer.addEventListener("scroll", handleScroll);
+}
