852190d1152e8d131bb9491c79f1361b471756bf
Leif Riksheim <riksheim.leif@gmail.com>
2021-08-23 15:44:53 +0200

    chore: put types in respective files

---
+++ src/store/types/neighbourhood.ts
@@ -0,0 +1,29 @@
+import type { LinkExpression } from "@perspect3vism/ad4m";
+import { PerspectiveHandle } from "@perspect3vism/ad4m";
+import { JuntoExpressionReference, ExpressionAndRef } from "./expression";
+import { ProfileExpression } from "./userprofile";
+
+export interface NeighbourhoodState {
+  name: string;
+  description: string;
+  creatorDid: string;
+  image?: string;
+  thumbnail?: string;
+  perspective: PerspectiveHandle;
+  typedExpressionLanguages: JuntoExpressionReference[];
+  groupExpressionRef?: string;
+  neighbourhoodUrl: string;
+  membraneType: MembraneType;
+  membraneRoot?: string;
+  linkedPerspectives: string[];
+  linkedNeighbourhoods: string[];
+  members: ProfileExpression[];
+  currentExpressionLinks: { [x: string]: LinkExpression };
+  currentExpressionMessages: { [x: string]: ExpressionAndRef };
+  createdAt?: Date;
+}
+
+export enum MembraneType {
+  Inherited,
+  Unique,
+}
---
+++ src/store/types/state.ts
@@ -0,0 +1,44 @@
+import { UserState } from "./userprofile";
+import { ExpressionUIIcons } from "./expression";
+import { LocalCommunityState } from "./community";
+import { LocalChannelState } from "./channel";
+import { NeighbourhoodState } from "./neighbourhood";
+import {
+  WindowState,
+  ModalsState,
+  CurrentThemeState,
+  ToastState,
+  ThemeState,
+  UpdateState,
+} from "./ui";
+
+export interface State {
+  app: ApplicationState;
+  data: DataState;
+  user: UserState;
+}
+
+export interface DataState {
+  communities: { [perspectiveUuid: string]: LocalCommunityState };
+  channels: { [perspectiveUuid: string]: LocalChannelState };
+  neighbourhoods: { [perspectiveUuid: string]: NeighbourhoodState };
+}
+
+export interface ApplicationState {
+  expressionUI: { [x: string]: ExpressionUIIcons };
+  localLanguagesPath: string;
+  databasePerspective?: string;
+  windowState: WindowState;
+  toast: ToastState;
+  applicationStartTime: Date;
+  updateState: UpdateState;
+  globalTheme: ThemeState;
+  currentTheme: CurrentThemeState;
+  modals: ModalsState;
+  showSidebar: boolean;
+  showGlobalLoading: boolean;
+  globalError: {
+    show: boolean;
+    message: string;
+  };
+}
