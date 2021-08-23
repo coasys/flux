461dc656f79ef08ffcf678cceca012d96277a2bb
Leif Riksheim <riksheim.leif@gmail.com>
2021-08-23 18:12:06 +0200

    chore: create constants for semantic data names

---
+++ src/constants/neighbourhoodMeta.ts
@@ -0,0 +1,9 @@
+export const DESCRIPTION = "rdf://description";
+
+export const SELF = "self";
+
+export const LANGUAGE = "language";
+
+export const CREATOR = "rdf://creator";
+
+export const NAME = "rdf://name";
