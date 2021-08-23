55310184fa78dfcbcf9d20e56336bdd4f3fba3a8
fayeed <fayeed@live.com>
2021-08-23 12:26:36 +0530

    fix(store): Moved global declaration to a seperate file

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,16 +1,7 @@
 /* eslint-disable */
-
-export {}
-
 declare module "*.vue" {
   import type { DefineComponent } from "vue";
   const component: DefineComponent<{}, {}, any>;
   export default component;
-}
-
-declare global {
-  interface Window {
-    api: any;
-  }
 }
 
