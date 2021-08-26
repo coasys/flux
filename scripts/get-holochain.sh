8be87a7da1577c2e83468ed2aea72fa60c159d3c
jdeepee <joshuadparkin@gmail.com>
2021-08-26 14:36:47 +0100

    use static lair version

--- scripts/get-holochain.sh
+++ scripts/get-holochain.sh
@@ -3,6 +3,7 @@
 [ ! -d "./resources/temp" ] && mkdir "./resources/temp"
 
 HOLOCHAIN_VER="f3d17d993ad8d988402cc01d73a0095484efbabb"
+LAIR_VERSION="6a9aab37c90566328c13c4d048d1afaf75fc39a9"
 
 echo "Getting holochain from GitHub"
 
@@ -10,3 +11,4 @@ echo "Getting holochain from GitHub"
 [ ! -d "./resources/temp/holochain" ] && git clone https://github.com/holochain/holochain ./resources/temp/holochain
 
 cd ./resources/temp/holochain && git checkout $HOLOCHAIN_VER
+cd ../temp/lair && git checkout $LAIR_VERSION
