a9713f39bb7a25a80c01877cc4d05e958ec008ef
jdeepee <joshuadparkin@gmail.com>
2021-09-30 16:46:05 +0100

    add button to copy logs to desktop for easy bug sharing

--- src/containers/Privacy.vue
+++ src/containers/Privacy.vue
@@ -8,15 +8,37 @@
       Clear state
     </j-button>
   </j-box>
+  <j-box pb="300">
+    <j-text variant="label">Get logs</j-text>
+  </j-box>
+  <j-button size="md" variant="primary" @click="getLogs">
+    <j-icon size="sm" name="clipboard"></j-icon>
+    Get logs
+  </j-button>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import { useAppStore } from "@/store/app";
 
 export default defineComponent({
+  setup() {
+    const appStore = useAppStore();
+
+    return {
+      appStore,
+    };
+  },
   methods: {
     cleanState() {
       window.api.send("cleanState");
+    },
+    getLogs() {
+      window.api.send("copyLogs");
+      this.appStore.showSuccessToast({
+        message:
+          "Log file called debug.log been copied to your desktop, please upload to Junto Discord, thanks <3",
+      });
     },
   },
 });
