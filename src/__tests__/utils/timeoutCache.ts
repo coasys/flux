52197e5523393799aadb776513b52380a2b8e071
jdeepee <joshuadparkin@gmail.com>
2021-10-13 20:56:29 +0100

    fix cache tests

--- src/__tests__/utils/timeoutCache.ts
+++ src/__tests__/utils/timeoutCache.ts
@@ -2,37 +2,37 @@ import sleep from "@/utils/sleep";
 import { TimeoutCache } from "@/utils/timeoutCache";
 
 describe("Timeoutcache", () => {
-  beforeEach(() => {
+  beforeEach(async () => {
     const cache = new TimeoutCache<any>(10);
 
-    cache.remove("test");
+    await cache.remove("test");
   });
 
   test("timeout cache is empty", async () => {
     const cache = new TimeoutCache<string>(10);
 
-    expect(cache.get("test")).toBe(undefined);
+    expect(await cache.get("test")).toBe(undefined);
   });
 
   test("timeout cache is not empty", async () => {
     const cache = new TimeoutCache<string>(10);
 
-    expect(cache.get("test")).toBe(undefined);
+    expect(await cache.get("test")).toBe(undefined);
 
-    cache.set("test", "test");
+    await cache.set("test", "test");
 
-    expect(cache.get("test")).toBe("test");
+    expect(await cache.get("test")).toBe("test");
   });
 
   test("timeout cache is empty after 10 ms", async () => {
     const cache = new TimeoutCache<string>(10);
 
-    expect(cache.get("test")).toBe(undefined);
+    expect(await cache.get("test")).toBe(undefined);
 
-    cache.set("test", "test");
+    await cache.set("test", "test");
 
     await sleep(20);
 
-    expect(cache.get("test")).toBe(undefined);
+    expect(await cache.get("test")).toBe(undefined);
   });
 });
