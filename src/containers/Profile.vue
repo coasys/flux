ff4971b082c283b2ce2265e64ba2f811bf9c80a1
Leif Riksheim <riksheim.leif@gmail.com>
2021-11-02 18:16:36 +0100

    fix: skeleton loader while fetching profile

--- src/containers/Profile.vue
+++ src/containers/Profile.vue
@@ -9,14 +9,22 @@
       <j-text variant="heading-sm"> {{ profile.username }}</j-text>
     </j-flex>
   </j-box>
+  <j-box p="800" v-else>
+    <j-flex a="center" direction="column" gap="500">
+      <Skeleton variant="circle" width="100px" height="100px" />
+      <Skeleton />
+    </j-flex>
+  </j-box>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import { getProfile } from "@/utils/profileHelpers";
 import { Profile } from "@/store/types";
+import Skeleton from "@/components/skeleton/Skeleton.vue";
 
 export default defineComponent({
+  components: { Skeleton },
   props: ["did", "langAddress"],
   data() {
     return {
@@ -25,9 +33,13 @@ export default defineComponent({
   },
   watch: {
     did: async function (did) {
+      // Reset profile while we get new data
+      this.profile = null;
       let profileLang = this.langAddress;
       this.profile = await getProfile(profileLang, did);
     },
   },
 });
 </script>
+
+<style></style>
