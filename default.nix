34ca7b59da78b73179742035a7a55227891f7722
jdeepee <joshuadparkin@gmail.com>
2022-01-22 17:27:04 +0000

    bump holochain version

--- default.nix
+++ default.nix
@@ -1,6 +1,5 @@
-
 {
-  holonixPath ?  builtins.fetchTarball { url = "https://github.com/holochain/holonix/archive/de9d6d1e820f4e3beeb20c24005c17f565a24453.tar.gz"; }
+  holonixPath ?  builtins.fetchTarball { url = "https://github.com/holochain/holonix/archive/2f7b8047d6314f64fca34394a52d465c18b2f4d5.tar.gz"; }
 }:
 
 let
