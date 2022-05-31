3b25cf09e27cabb4fd184d53d691761b0033ba9a
fayeed <fayeed@live.com>
2022-05-31 16:43:49 +0530

    Signup add agent data

--- src/store/user/actions/createUser.ts
+++ src/store/user/actions/createUser.ts
@@ -1,6 +1,10 @@
 import { ad4mClient } from "@/app";
+import { NOTE_IPFS_EXPRESSION_OFFICIAL } from "@/constants/languages";
+import { blobToDataURL, dataURItoBlob, resizeImage } from "@/core/methods/createProfile";
 
 import { useAppStore } from "@/store/app";
+import removeTypeName from "@/utils/removeTypeName";
+import { Link, PerspectiveInput } from "@perspect3vism/ad4m";
 import { useUserStore } from "..";
 
 export interface Payload {
@@ -14,19 +18,121 @@ export interface Payload {
 }
 
 export default async ({
-  givenName,
-  familyName,
-  email,
+  givenName = "",
+  familyName = "",
+  email = "",
   username,
   password,
-  profilePicture,
-  thumbnailPicture,
+  profilePicture
 }: Payload): Promise<void> => {
   const appStore = useAppStore();
   const userStore = useUserStore();
 
   try {
+    const resizedImage = profilePicture
+      ? await resizeImage(dataURItoBlob(profilePicture as string), 100)
+      : undefined;
+    
+    const thumbnail = profilePicture
+      ? await blobToDataURL(resizedImage!)
+      : undefined;
+
     const status = await ad4mClient.agent.generate(password);
+    const userPerspective = await (await ad4mClient.perspective.all()).find(e => e.name === "My flux perspective")?.uuid;
+    const tempLinks = [];
+    
+    if (profilePicture) {
+      const thumbnailImage = await ad4mClient.expression.create(
+        thumbnail,
+        NOTE_IPFS_EXPRESSION_OFFICIAL
+      );
+
+      const profileImage = await ad4mClient.expression.create(
+        profilePicture,
+        NOTE_IPFS_EXPRESSION_OFFICIAL
+      );
+
+      const profileImageLink = await ad4mClient.perspective.addLink(
+        userPerspective!,
+        new Link({
+          source: "flux://profile",
+          target: profileImage,
+          predicate: `sioc://has_profile_image`,
+        })
+      );
+  
+      const thumbnailImageLink = await ad4mClient.perspective.addLink(
+        userPerspective!,
+        new Link({
+          source: "flux://profile",
+          target: thumbnailImage,
+          predicate: `sioc://has_profile_thumbnail_image`,
+        })
+      );
+
+      tempLinks.push(profileImageLink, thumbnailImageLink);
+    }
+
+    if (givenName) {
+      const givenNameLink = await ad4mClient.perspective.addLink(
+        userPerspective!,
+        new Link({
+          source: "flux://profile",
+          target: givenName,
+          predicate: "sioc://has_given_name",
+        })
+      );
+
+      tempLinks.push(givenNameLink);
+    }
+
+    if (familyName) {
+      const familyNameLink = await ad4mClient.perspective.addLink(
+        userPerspective!,
+        new Link({
+          source: "flux://profile",
+          target: familyName,
+          predicate: "sioc://has_family_name",
+        })
+      );
+
+      tempLinks.push(familyNameLink);
+    }
+
+    if (email) {
+      const emailLink = await ad4mClient.perspective.addLink(
+        userPerspective!,
+        new Link({
+          source: "flux://profile",
+          target: email,
+          predicate: "sioc://has_email",
+        })
+      );
+
+      tempLinks.push(emailLink);
+    }
+
+    if (username) {
+      const userNameLink = await ad4mClient.perspective.addLink(
+        userPerspective!,
+        new Link({
+          source: "flux://profile",
+          target: username,
+          predicate: "sioc://has_username",
+        })
+      );
+
+      tempLinks.push(userNameLink);
+    }
+
+    const links = [];
+    for (const link of tempLinks) {
+      links.push(removeTypeName(link));
+    }
+
+    await ad4mClient.agent.updatePublicPerspective({
+      links,
+    } as PerspectiveInput);
 
     userStore.setUserProfile({
       username: username,
@@ -34,7 +140,8 @@ export default async ({
       givenName: givenName,
       familyName: familyName,
       profilePicture,
-      thumbnailPicture,
+      thumbnailPicture: thumbnail,
+      bio: ""
     });
     userStore.updateAgentStatus(status);
   } catch (e) {
