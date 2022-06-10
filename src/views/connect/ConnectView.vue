7409b2c39d7f27110505226429c9304136348627
Leif Riksheim <riksheim.leif@gmail.com>
2022-06-10 14:08:21 +0200

    rename and delete things we don't need anymore

---
+++ src/views/connect/ConnectView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>Hello</div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  name: "ChannelView",
+  components: {},
+  setup() {
+    return {};
+  },
+});
+</script>
