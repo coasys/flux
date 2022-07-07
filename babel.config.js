c3f9addd241d60abbac5d809e75cd16599f3aaf2
Fayeed Pawaskar <fayeed@live.com>
2022-07-07 11:59:42 +0530

    Removed unused dependency

--- babel.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  presets: [
-    "@babel/preset-typescript",
-  ],
-  plugins: ["@babel/plugin-transform-runtime"],
-  ignore: [/\/node_modules\//],
-  exclude: [/\bcore-js\b/],
-};
