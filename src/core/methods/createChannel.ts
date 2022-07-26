a417898583a8736bd83c3a080c5e95006f490ec6
Fayeed Pawaskar <fayeed@live.com>
2022-07-26 15:44:48 +0530

    Changed channel name as unique identifier

--- src/core/methods/createChannel.ts
+++ src/core/methods/createChannel.ts
@@ -7,7 +7,8 @@ import {
 import type { PerspectiveHandle } from "@perspect3vism/ad4m";
 import { ad4mClient } from "@/app";
 import { CHANNEL } from "@/constants/neighbourhoodMeta";
-import { nanoid } from "nanoid";
+import { useDataStore } from "@/store/data";
+import { useAppStore } from "@/store/app";
 
 interface ChannelProps {
   channelName: string;
@@ -20,55 +21,56 @@ export async function createChannel({
   creatorDid,
   sourcePerspective,
 }: ChannelProps): Promise<ChannelState> {
-  const id = nanoid();
+  const dataStore = useDataStore();
+  const appStore = useAppStore();
 
-  const addLinkToChannel = await ad4mClient.perspective.addLink(
-    sourcePerspective.uuid,
-    {
-      source: sourcePerspective.sharedUrl!,
-      target: id,
-      predicate: CHANNEL,
-    }
-  );
-  
-  console.debug(
-    "Created new link on source social-context with result",
-    addLinkToChannel
-  );
+  const channel = dataStore.channels[channelName];
+  if (!channel || channel.sourcePerspective !== sourcePerspective.uuid) {
+    const addLinkToChannel = await ad4mClient.perspective.addLink(
+      sourcePerspective.uuid,
+      {
+        source: sourcePerspective.sharedUrl!,
+        target: channelName,
+        predicate: CHANNEL,
+      }
+    );
 
-  const channelNameLink = await ad4mClient.perspective.addLink(
-    sourcePerspective.uuid,
-    {
-      source: addLinkToChannel.data.target,
-      target: channelName,
-      predicate: 'flux://name',
-    }
-  );
+    console.debug(
+      "Created new link on source social-context with result",
+      addLinkToChannel
+    );
 
-  console.log("Created channel name link", channelNameLink);
+    const channelCreatorDidLink = await ad4mClient.perspective.addLink(
+      sourcePerspective.uuid,
+      {
+        source: addLinkToChannel.data.target,
+        target: creatorDid,
+        predicate: 'flux://creator_did',
+      }
+    );
 
-  const channelCreatorDidLink = await ad4mClient.perspective.addLink(
-    sourcePerspective.uuid,
-    {
-      source: addLinkToChannel.data.target,
-      target: creatorDid,
-      predicate: 'flux://creator_did',
-    }
-  );
+    console.log("Created channel creator did link", channelCreatorDidLink);
 
-  console.log("Created channel creator did link", channelCreatorDidLink);
+    return {
+      name: channelName,
+      description: "",
+      creatorDid,
+      id: channelName,
+      createdAt: new Date().toISOString(),
+      sourcePerspective: sourcePerspective.uuid,
+      hasNewMessages: false,
+      feedType: FeedType.Signaled,
+      notifications: {
+        mute: false,
+      },
+    } as ChannelState;
+  }
 
-  return {
-    name: channelName,
-    description: "",
-    creatorDid,
-    id,
-    createdAt: new Date().toISOString(),
-    sourcePerspective: sourcePerspective.uuid,
-    hasNewMessages: false,
-    feedType: FeedType.Signaled,
-    notifications: {
-      mute: false,
-    },
-  } as ChannelState;
+  const errorMessage = 'Channel with this name already exists';
+
+  appStore.showDangerToast({
+    message: errorMessage,
+  });
+
+  throw new Error(errorMessage)
 }
