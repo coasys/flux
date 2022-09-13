3513aa657bb6fa0651657ae52bdfe64336ed249f
Leif Riksheim <riksheim.leif@gmail.com>
2022-09-13 12:26:00 +0200

    chore: add types for explanation

--- src/utils/linkHelpers.ts
+++ src/utils/linkHelpers.ts
@@ -1,9 +1,16 @@
 import { LinkExpression, Literal } from "@perspect3vism/ad4m";
 
+type MapKey = string;
+type Predicate = string;
+
+type Map = {
+  [x: MapKey]: Predicate;
+};
+
 export function mapLiteralLinks(
   links: LinkExpression[] | undefined,
-  map: any
-): any {
+  map: Map
+): Map {
   return Object.keys(map).reduce((acc, key) => {
     const predicate = map[key];
     const link = links?.find((link) => link.data.predicate === predicate);
