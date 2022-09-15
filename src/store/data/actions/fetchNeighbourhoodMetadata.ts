98df26333f96020751937905a0a0cd2bd58f710a
jdeepee <joshuadparkin@gmail.com>
2022-09-15 12:52:50 +0100

    fix multiple prolog query

--- src/store/data/actions/fetchNeighbourhoodMetadata.ts
+++ src/store/data/actions/fetchNeighbourhoodMetadata.ts
@@ -1,7 +1,5 @@
-import { LinkQuery } from "@perspect3vism/ad4m";
-
 import { useDataStore } from "@/store/data/index";
-import { SELF, FLUX_GROUP_NAME, FLUX_GROUP_DESCRIPTION, FLUX_CHANNEL, FLUX_GROUP_IMAGE, FLUX_GROUP_THUMBNAIL } from "@/constants/neighbourhoodMeta";
+import { SELF, FLUX_GROUP_NAME, FLUX_GROUP_DESCRIPTION, FLUX_GROUP_IMAGE, FLUX_GROUP_THUMBNAIL } from "@/constants/neighbourhoodMeta";
 import { DexieIPFS } from "@/utils/storageHelpers";
 import { getImage } from "../../../utils/profileHelpers";
 import { getAd4mClient } from "@perspect3vism/ad4m-connect/dist/web";
@@ -10,42 +8,18 @@ export interface Payload {
   communityId: string;
 }
 
-export async function getGroupExpression(communityId: string) {
+export async function getGroupMetadata(communityId: string) {
   const client = await getAd4mClient();
   const dexie = new DexieIPFS(communityId);
-
-  const groupNameLink = client.perspective.queryLinks(
-    communityId,
-    new LinkQuery({
-      source: SELF,
-      predicate: FLUX_GROUP_NAME,
-    })
-  );
-  const groupDescriptionLink = client.perspective.queryLinks(
-    communityId,
-    new LinkQuery({
-      source: SELF,
-      predicate: FLUX_GROUP_DESCRIPTION,
-    })
-  );
-  const groupImageLink = client.perspective.queryLinks(
-    communityId,
-    new LinkQuery({
-      source: SELF,
-      predicate: FLUX_GROUP_IMAGE,
-    })
-  );
-  const groupThumbnailLink = client.perspective.queryLinks(
-    communityId,
-    new LinkQuery({
-      source: SELF,
-      predicate: FLUX_GROUP_THUMBNAIL,
-    })
-  );
-
-  const values = await Promise.all([groupNameLink, groupDescriptionLink, groupImageLink, groupThumbnailLink])
-  const links = values.flat()
+  
+  const groupMetaData = await client.perspective.queryProlog(communityId, `
+  triple("${SELF}", "${FLUX_GROUP_NAME}", GN);
+  triple("${SELF}", "${FLUX_GROUP_DESCRIPTION}", GD);
+  triple("${SELF}", "${FLUX_GROUP_IMAGE}", GI);
+  triple("${SELF}", "${FLUX_GROUP_THUMBNAIL}", GT).`);
+  
   const group = {
+    communityId,
     name: '',
     description: '',
     image: null,
@@ -53,23 +27,23 @@ export async function getGroupExpression(communityId: string) {
   };
 
 
-  for (const link of links) {
-    if (link.data.predicate === FLUX_GROUP_NAME) {
-      group.name = link.data.target
-    } else if (link.data.predicate === FLUX_GROUP_DESCRIPTION) {
-      group.description = link.data.target
-    } else if (link.data.predicate === FLUX_GROUP_IMAGE) {
-      const image = await getImage(link.data.target);
+  for (const link of groupMetaData) {
+    if (typeof link.GN == 'string') {
+      group.name = link.GN
+    } else if (typeof link.GD == 'string') {
+      group.description = link.GD
+    } else if (typeof link.GI == 'string') {
+      const image = await getImage(link.GI);
   
-      await dexie.save(link.data.target, image);
+      await dexie.save(link.GI, image);
 
-      group.image = link.data.target;
-    } else if (link.data.predicate === FLUX_GROUP_THUMBNAIL) {
-      const image = await getImage(link.data.target);
+      group.image = link.GI;
+    } else if (typeof link.GT == 'string') {
+      const image = await getImage(link.GT);
   
-      await dexie.save(link.data.target, image);
+      await dexie.save(link.GT, image);
 
-      group.thumbnail = link.data.target;
+      group.thumbnail = link.GT;
     }
   }
 
@@ -79,7 +53,7 @@ export async function getGroupExpression(communityId: string) {
 export default async (communityId: string): Promise<void> => {
   const dataStore = useDataStore();
 
-  const exp = await getGroupExpression(communityId);
+  const exp = await getGroupMetadata(communityId);
 
   if (exp) {
     dataStore.updateCommunityMetadata(exp);
