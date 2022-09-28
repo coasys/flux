de9e91acae93e48387d50d326e08f7d82af6a1fc
Leif Riksheim <riksheim.leif@gmail.com>
2022-09-28 14:16:47 +0200

    showMainSidebar toggle

--- src/layout/AppLayout.vue
+++ src/layout/AppLayout.vue
@@ -1,9 +1,24 @@
 <template>
-  <div class="app-layout">
+  <div
+    class="app-layout"
+    :class="{ 'app-layout--show-sidebar': appStore.showMainSidebar }"
+  >
     <div class="app-layout__sidebar"><slot name="sidebar"></slot></div>
     <main class="app-layout__main"><slot></slot></main>
   </div>
 </template>
+
+<script lang="ts">
+import { useAppStore } from "@/store/app";
+
+export default {
+  setup() {
+    return {
+      appStore: useAppStore(),
+    };
+  },
+};
+</script>
 
 <style>
 .app-layout {
@@ -11,7 +26,13 @@
   display: grid;
   grid-template-columns: var(--app-main-sidebar-width) 1fr;
 }
+
+.app-layout--show-sidebar .app-layout__sidebar {
+  display: flex;
+}
+
 .app-layout__sidebar {
+  display: none;
   background: var(--app-main-sidebar-bg-color);
   height: 100%;
   border-right: 1px var(--app-main-sidebar-border-color) solid;
