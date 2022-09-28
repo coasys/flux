0d779175bf68e891d212751de35cfaaedcf1ca39
Leif Riksheim <riksheim.leif@gmail.com>
2022-09-28 18:33:28 +0200

    animation on menu hide

--- src/layout/AppLayout.vue
+++ src/layout/AppLayout.vue
@@ -28,23 +28,36 @@ export default {
 }
 
 .app-layout--show-sidebar {
-  grid-template-columns: var(--app-main-sidebar-width) 1fr;
+  grid-template-columns: 1fr;
 }
 
 .app-layout--show-sidebar .app-layout__sidebar {
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
+  transform: translateX(0px);
+}
+
+.app-layout--show-sidebar .app-layout__main {
+  margin-left: var(--app-main-sidebar-width);
 }
 
 .app-layout__sidebar {
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: var(--app-main-sidebar-width);
+  display: flex;
+  z-index: 999;
+  flex-direction: column;
+  justify-content: space-between;
   background: var(--app-main-sidebar-bg-color);
   height: 100%;
   border-right: 1px var(--app-main-sidebar-border-color) solid;
-  display: none;
+  transition: all 0.3s ease;
+  transform: translateX(calc(var(--app-main-sidebar-width) * -1));
 }
 .app-layout__main {
   height: 100%;
   z-index: 0;
+  transition: all 0.3s ease;
+  margin-left: 0;
 }
 </style>
