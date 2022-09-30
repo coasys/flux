ef31ac94e6231617ada18a09347bdc7cbc8fd122
Fayeed <fayeed@live.com>
2022-09-30 18:13:50 +0530

    Privacy page updated

--- src/containers/Privacy.vue
+++ src/containers/Privacy.vue
@@ -1,40 +1,12 @@
 <template>
   <j-box pb="800">
     <j-box pb="300">
-      <j-text variant="label">Clear State</j-text>
+      <j-text variant="label">Flux Privacy Policy</j-text>
     </j-box>
-    <j-button size="md" variant="primary" @click="showConfirm = true">
-      <j-icon size="sm" name="trash"></j-icon>
-      Clear state
+    <j-button size="md" variant="primary" @click="changeNotificationState">
+      {{this.appStore.notification.globalNotification ? 'Disable' : 'Enable'}} Notifications
     </j-button>
   </j-box>
-  <j-box pb="300">
-    <j-text variant="label">Get logs</j-text>
-  </j-box>
-  <j-button size="md" variant="primary" @click="getLogs">
-    <j-icon size="sm" name="clipboard"></j-icon>
-    Get logs
-  </j-button>
-  <j-modal :open="showConfirm" @toggle="(e) => (showConfirm = e.target.open)">
-    <j-box p="800">
-      <j-box py="500">
-        <j-text variant="heading-sm">
-          Are you sure you want to clear your state?
-        </j-text>
-        <j-text variant="body">
-          This means you will loose all your communities and messages
-        </j-text>
-      </j-box>
-      <j-box pt="500">
-        <j-flex gap="300">
-          <j-button @click="showConfirm = false">Cancel</j-button>
-          <j-button variant="primary" @click="cleanState">
-            Delete everything
-          </j-button>
-        </j-flex>
-      </j-box>
-    </j-box>
-  </j-modal>
 </template>
 
 <script lang="ts">
@@ -51,15 +23,8 @@ export default defineComponent({
     };
   },
   methods: {
-    cleanState() {
-      window.api.send("cleanState");
-    },
-    getLogs() {
-      window.api.send("copyLogs");
-      this.appStore.showSuccessToast({
-        message:
-          "Log file called debug.log been copied to your desktop, please upload to Junto Discord, thanks <3",
-      });
+    changeNotificationState() {
+      this.appStore.changeNotificationState(!this.appStore.notification.globalNotification);
     },
   },
 });
