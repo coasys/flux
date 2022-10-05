c2ec462d691236fc29e0d40ac95b281df67586d9
Fayeed <fayeed@live.com>
2022-10-05 19:47:58 +0530

    Fixed the get meta link

--- src/core/methods/getMetaFromNeighbourhood.ts
+++ src/core/methods/getMetaFromNeighbourhood.ts
@@ -1,4 +1,4 @@
-import { LinkExpression } from "@perspect3vism/ad4m";
+import { LinkExpression, Literal } from "@perspect3vism/ad4m";
 import {
   DESCRIPTION,
   NAME,
@@ -16,8 +16,8 @@ export function getMetaFromNeighbourhood(links: LinkExpression[]): {
     (acc, link) => {
       const { predicate, target } = link.data;
       return {
-        name: predicate === NAME ? target : acc.name,
-        description: predicate === DESCRIPTION ? target : acc.description,
+        name: predicate === NAME ? Literal.fromUrl(target).get().data : acc.name,
+        description: predicate === DESCRIPTION ? Literal.fromUrl(target).get().data : acc.description,
         creatorDid: predicate === CREATOR ? target : acc.creatorDid,
         createdAt: predicate === CREATED_AT ? target : acc.createdAt,
       };
