366f9d1f553d82c00d5fa8fa469acf7bd2639c05
Joshua Parkin <josh@Joshuas-MacBook-Pro.local>
2022-10-14 17:22:08 +0100

    first pass at basic emoji author pop up indicator

--- packages/chat-view/src/components/MessageItem/MessageReactions.tsx
+++ packages/chat-view/src/components/MessageItem/MessageReactions.tsx
@@ -1,9 +1,17 @@
 import { AgentContext } from "utils/react";
 import { Reaction } from "utils/types";
-import { useContext } from "preact/hooks";
+import { useContext, useEffect, useState } from "preact/hooks";
 import styles from "./index.scss";
+import getProfile from "utils/api/getProfile";
+import { getAd4mClient } from "@perspect3vism/ad4m-connect/dist/utils";
 
-function sortReactions(reactions: Reaction[]) {
+interface ReactionType {
+  authors: string[];
+  content: string;
+  count: number;
+}
+
+function sortReactions(reactions: Reaction[]): ReactionType[] {
   const mapped = reactions.reduce((acc: any, reaction) => {
     const previous = acc[reaction.content] || { authors: [], count: 0 };
     return {
@@ -13,38 +21,89 @@ function sortReactions(reactions: Reaction[]) {
         content: reaction.content,
         count: previous.count + 1,
       },
-    };
+    } as ReactionType;
   }, {});
   return Object.values(mapped);
+}
+
+function generateReactionText(reaction: ReactionType, profiles: any[], me: string) {
+  if (profiles.length) {
+    const emoji = String.fromCodePoint(parseInt(`0x${reaction.content}`));
+
+    if (reaction.authors.find(author => author === me)) {
+      const authors = reaction.authors.filter(author => author !== me);
+      if (authors.length > 2) {
+        return `You, ${profiles[0].username}, ${profiles[1].username}, and ${authors.length - 3} others`;
+      } else if (authors.length == 2) {
+        return `You, ${profiles[0].username} and ${profiles[1].username}`;
+      } else if (authors.length == 1) {
+        return `You and ${profiles[0].username}`;
+      } else {
+        return `You`;
+      }
+    } else {
+      if (reaction.authors.length > 3) {
+        return `${profiles[0].username}, ${profiles[1].username}, ${profiles[2].username}, and ${reaction.authors.length - 3} others reacted with ${emoji}`;
+      } else if (reaction.authors.length == 3) {
+        return `${profiles[0].username}, ${profiles[1].username} and ${profiles[2].username} reacted with ${emoji}`;
+      } else if (reaction.authors.length == 2) {
+        return `${profiles[0].username} and ${profiles[1].username} reacted with ${emoji}`;
+      } else if (reaction.authors.length == 1) {
+        return `${profiles[0].username} reacted with ${emoji}`;
+      }
+    }
+  } else {
+    return "";
+  }
 }
 
 export default function MessageReactions({ onEmojiClick, reactions = [] }) {
   const sortedReactions = sortReactions(reactions);
 
-  const { state: agentState } = useContext(AgentContext);
-
   return (
     <div style={{ display: "flex", gap: "var(--j-space-200)" }}>
       {sortedReactions.map((reaction: any, i) => {
-        const activeClass = reaction.authors.find(
-          (did) => did === agentState.did
-        )
-          ? styles.emojiButtonActive
-          : "";
-
-        return (
-          <button
-            class={`${styles.emojiButton} ${activeClass}`}
-            onClick={() => onEmojiClick(reaction.content)}
-            key={i}
-          >
-            <span>
-              {String.fromCodePoint(parseInt(`0x${reaction.content}`))}
-            </span>
-            <span>{reaction.count}</span>
-          </button>
-        );
+        return ReactionButton({ reaction, onEmojiClick, key: i });
       })}
     </div>
+  );
+}
+
+function ReactionButton({ reaction, onEmojiClick }) {
+  const { state: agentState } = useContext(AgentContext);
+  const activeClass = reaction.authors.find((did) => did === agentState.did)
+    ? styles.emojiButtonActive
+    : "";
+
+  const [profiles, setProfiles] = useState([]);
+
+  async function getProfiles(){
+    const profiles = await Promise.all(
+      reaction.authors.filter(author => author !== agentState.did).slice(0, 3).map((did) =>
+        getProfile(did)
+      )
+    );
+    setProfiles(profiles);
+  };
+
+  console.log(profiles);
+
+  return (
+    <j-popover event="mouseover">
+      <button
+        class={`${styles.emojiButton} ${activeClass}`}
+        onClick={() => onEmojiClick(reaction.content)}
+        slot="trigger"
+        onMouseOver = {() => getProfiles()}
+      >
+        <span>
+          {String.fromCodePoint(parseInt(`0x${reaction.content}`))}
+        </span>
+        <span>{reaction.count}</span>
+      </button>
+      <j-box slot="content" bg="ui-100">
+        <j-text>{generateReactionText(reaction, profiles, agentState.did)}</j-text>
+      </j-box>
+    </j-popover>
   );
 }
