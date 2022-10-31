b208605cf6d6ca50c7e485e634829d91d6a84f17
Leif Riksheim <riksheim.leif@gmail.com>
2022-10-31 10:11:23 +0100

    chore: add styles on neighbourhoodcard

--- packages/chat-view/src/components/MessageItem/NeighbourhoodCard.tsx
+++ packages/chat-view/src/components/MessageItem/NeighbourhoodCard.tsx
@@ -4,17 +4,15 @@ export default function NeighbourhoodCard({ name, description, onClick }) {
   return (
     <div class={styles.neighbourhoodCard} size="300" onClick={onClick}>
       <div>
-        <div>
-          <div>Neighbourhood</div>
-          <div nomargin color="black" size="500" weight="600">
-            {name || "Unknown Community"}
-          </div>
-          <div nomargin size="500" weight="400" color="ui-600">
-            {description || "No descriptioon"}
-          </div>
+        <small class={styles.neighbourhoodCardTitle}>Neighbourhood</small>
+        <div class={styles.neighbourhoodCardName}>
+          {name || "Unknown Community"}
         </div>
-        <j-button variant="primary">Join</j-button>
+        <div class={styles.neighbourhoodCardDescription}>
+          {description || "No descriptioon"}
+        </div>
       </div>
+      <j-button variant="primary">Join</j-button>
     </div>
   );
 }
