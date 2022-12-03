1a5026e6f54b781d5e7b8e33d5e34b08cb4fc1e7
Joshua Parkin <joshuadparkin@gmail.com>
2022-12-03 12:40:46 -0800

    use newly publish link and ipfs languages for new language language

--- packages/utils/constants/languages.ts
+++ packages/utils/constants/languages.ts
@@ -1,5 +1,5 @@
 export const PERSPECTIVE_DIFF_SYNC =
-  "QmeW1pqaij9dgy4g7t4o1R6UMZH6DffuGoUUL3cwWDuAVz";
+  "QmSNuYg3jmCfQ3xYaPpk5uQEYzkzkm5qjLvZmVhesxi3ny";
 
 export const NOTE_IPFS_EXPRESSION_OFFICIAL =
-  "QmYVsrMpiFmV9S7bTWNAkUzSqjRJskQ8g4TWKKwKrHAPqL";
+  "QmbWg5VBFB1Zzce8X33GiGpMDXFPQjFQKS2T2rJtSYt7TJ";
