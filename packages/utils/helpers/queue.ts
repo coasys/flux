aae63dddcddd6e6fa74dbca283d49dc57f899a56
Leif Riksheim <riksheim.leif@gmail.com>
2022-12-19 19:11:42 +0100

    chore: safari support

--- packages/utils/helpers/queue.ts
+++ packages/utils/helpers/queue.ts
@@ -1,39 +1,39 @@
 class Queue<T> {
-  #queue: T[];
+  private queue: T[];
 
   constructor() {
-    this.#queue = [];
+    this.queue = [];
   }
 
   enqueue(func: T) {
-    this.#queue.push(func);
+    this.queue.push(func);
   }
 
   dequeue() {
-    return this.#queue.shift();
+    return this.queue.shift();
   }
 
   get size(): number {
-    return this.#queue.length;
+    return this.queue.length;
   }
 }
 
 export class AsyncQueue {
-  #queue = new Queue<() => any>();
-  #pendingCount = 0;
+  private queue = new Queue<() => any>();
+  private pendingCount = 0;
 
   private next() {
-    this.#pendingCount--;
+    this.pendingCount--;
     this.startAnotherFunc();
   }
 
   private startAnotherFunc(): boolean {
-    if (this.#queue.size === 0) {
+    if (this.queue.size === 0) {
       return false;
     }
 
-    if (this.#pendingCount === 0) {
-      const func = this.#queue.dequeue();
+    if (this.pendingCount === 0) {
+      const func = this.queue.dequeue();
 
       if (!func) {
         return false;
@@ -50,28 +50,28 @@ export class AsyncQueue {
   async add(func: () => void) {
     return new Promise((resolve, reject) => {
       const job = async () => {
-        this.#pendingCount++;
+        this.pendingCount++;
 
         try {
           const result = await func();
           resolve(result);
-        } catch(error) {
+        } catch (error) {
           reject(error);
         }
 
         this.next();
-      }
+      };
 
-      this.#queue.enqueue(job);
+      this.queue.enqueue(job);
       this.startAnotherFunc();
     });
   }
 
   async addAll(funcs: Array<() => void>) {
-    return Promise.all(funcs.map(async func => this.add(func)))
+    return Promise.all(funcs.map(async (func) => this.add(func)));
   }
 
   get pendingCount(): number {
-    return this.#pendingCount;
+    return this.pendingCount;
   }
 }
