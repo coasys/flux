cbab86ed79b185c2ddc6312a6f6722eae9dfd221
Leif Riksheim <riksheim.leif@gmail.com>
2022-12-20 16:04:46 +0100

    chore: new testing community

--- packages/utils/constants/general.ts
+++ packages/utils/constants/general.ts
@@ -1,5 +1,6 @@
 export const EXPECTED_AD4M_VERSION = "0.2.5";
 
-export const DEFAULT_TESTING_NEIGHBOURHOOD = "neighbourhood://QMA";
+export const DEFAULT_TESTING_NEIGHBOURHOOD =
+  "neighbourhood://QmY667koiPUuTQbPmDP4BMQEgrESFZA8LpYdg5n42h7Kap";
 
 export const COMMUNITY_TEST_VERSION = 1;
