4c750761229e8fbe6000396f0f3642d48342137f
Leif Riksheim <riksheim.leif@gmail.com>
2022-12-21 17:23:40 +0100

    update default

--- packages/utils/constants/general.ts
+++ packages/utils/constants/general.ts
@@ -1,6 +1,6 @@
 export const EXPECTED_AD4M_VERSION = "0.2.5";
 
 export const DEFAULT_TESTING_NEIGHBOURHOOD =
-  "neighbourhood://QmY667koiPUuTQbPmDP4BMQEgrESFZA8LpYdg5n42h7Kap";
+  "neighbourhood://QmeujoNhLrNGWVkV7Zg4c8HXjSzSQboD2Lh68ycbrguUPt";
 
 export const COMMUNITY_TEST_VERSION = 1;
