2261800673b19dab532fda392e2037113437dde0
Bård N. Hovde <bnhovde@gmail.com>
2023-01-06 11:42:18 +0100

    Added html template string minification to web components

--- packages/web-components/esbuild.js
+++ packages/web-components/esbuild.js
@@ -1,12 +1,32 @@
-require("esbuild")
-  .build({
-    entryPoints: ["./src/main.ts"],
-    bundle: true,
-    format: "esm",
-    minify: true,
-    sourcemap: true,
-    target: "safari11",
-    outfile: "dist/main.js",
-    watch: process.env.NODE_ENV === "dev" ? true : false,
-  })
-  .catch(() => process.exit(1));
+(async () => {
+  let esbuild = require("esbuild");
+  let {
+    minifyHTMLLiteralsPlugin,
+  } = require("esbuild-plugin-minify-html-literals");
+
+  const isDev = process.env.NODE_ENV === "dev";
+
+  let result = await esbuild
+    .build({
+      entryPoints: ["./src/main.ts"],
+      metafile: !isDev,
+      bundle: true,
+      format: "esm",
+      minify: true,
+      sourcemap: true,
+      target: isDev ? "safari11" : "es2016",
+      outfile: "dist/main.js",
+      watch: !!isDev,
+      plugins: isDev ? [] : [minifyHTMLLiteralsPlugin()],
+    })
+    .catch(() => process.exit(1));
+
+  if (!isDev) {
+    // Report results
+    let resultText = await esbuild.analyzeMetafile(result.metafile);
+    console.log(resultText);
+
+    // Create meta.json file
+    require("fs").writeFileSync("meta.json", JSON.stringify(result.metafile));
+  }
+})();
