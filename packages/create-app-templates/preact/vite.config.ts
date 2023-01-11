5d109c92baafc2c10dd4b9bb5d17528ab68b5a91
Leif Riksheim <riksheim.leif@gmail.com>
2023-01-11 13:11:54 +0100

    Update build step

--- packages/create-app-templates/preact/vite.config.ts
+++ packages/create-app-templates/preact/vite.config.ts
@@ -1,6 +1,15 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 import preact from "@preact/preset-vite";
+import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 
 export default defineConfig({
-  plugins: [preact()],
+  plugins: [preact(), cssInjectedByJsPlugin()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "./src/main.ts"),
+      name: "Main",
+      fileName: "main",
+    },
+  },
 });
