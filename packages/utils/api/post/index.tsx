2a7ea8efbc070f8182958d1825a0357d47c2b8e7
Nicolas Luck <nicolas@lucksus.eu>
2023-01-16 18:06:33 +0000

    Migrate PostModel to Subject class

--- packages/utils/api/post/index.tsx
+++ packages/utils/api/post/index.tsx
@@ -2,95 +2,55 @@ import {
   BODY,
   END_DATE,
   IMAGE,
-  REPLY_TO,
   START_DATE,
   TITLE,
   URL,
+  ENTRY_TYPE,
 } from "../../constants/communityPredicates";
 import { NOTE_IPFS_EXPRESSION_OFFICIAL } from "../../constants/languages";
-import EntryModel from "../../helpers/model";
-import { EntryType, Post } from "../../types";
-import MessageModel from "../message";
+import { EntryType } from "../../types";
+import { subjectProperty, subjectPropertySetter, subjectCollection } from "@perspect3vism/ad4m";
 
-class PostModel extends EntryModel {
-  static get type() {
-    return EntryType.Post;
-  }
-  static get properties() {
-    return {
-      title: {
-        predicate: TITLE,
-        type: String,
-        resolve: true,
-        languageAddress: "literal",
-      },
-      body: {
-        predicate: BODY,
-        type: String,
-        resolve: true,
-        languageAddress: "literal",
-      },
-      image: {
-        predicate: IMAGE,
-        type: String,
-        resolve: false,
-        languageAddress: NOTE_IPFS_EXPRESSION_OFFICIAL,
-      },
-      startDate: {
-        predicate: START_DATE,
-        type: String,
-        resolve: true,
-        languageAddress: "literal",
-      },
-      endDate: {
-        predicate: END_DATE,
-        type: String,
-        resolve: true,
-        languageAddress: "literal",
-      },
-      url: {
-        predicate: URL,
-        type: String,
-        resolve: true,
-        languageAddress: "literal",
-      },
-      comments: {
-        predicate: EntryType.Message,
-        type: String,
-        resolve: false,
-        collection: true,
-      },
-    };
-  }
+export class Post {
+  @subjectProperty({
+    through: ENTRY_TYPE, 
+    initial: EntryType.Post,
+    required: true,
+  })
 
-  create(data: {
-    title: string;
-    image?: string;
-    body?: string;
-    startDate?: string;
-    endDate?: string;
-    url?: string;
-  }): Promise<Post> {
-    return super.create(data) as Promise<Post>;
-  }
+  @subjectProperty({through: TITLE, resolve: true})
+  title: string;
+  @subjectPropertySetter({resolveLanguage: 'literal'})
+  setTitle(title: string) {}
 
-  createComment(replyTo: string, body: string) {
-    // TODO: Add a link from replyTo with Predicate
-    // REPLY_TO and the new message.id as target
-    const Message = new MessageModel({
-      perspectiveUuid: this.perspectiveUuid,
-      source: replyTo,
-    });
-    return Message.create({ body });
-  }
+  @subjectProperty({through: BODY, resolve: true})
+  body: string;
+  @subjectPropertySetter({resolveLanguage: 'literal'})
+  setBody(body: string) {}
 
-  getComments(id: string) {
-    const Message = new MessageModel({
-      perspectiveUuid: this.perspectiveUuid,
-      source: id,
-    });
-    return Message.getAll();
-  }
+  @subjectProperty({through: IMAGE})
+  image: string;
+  @subjectPropertySetter({resolveLanguage: NOTE_IPFS_EXPRESSION_OFFICIAL})
+  setImage(image: string) {}
+
+  @subjectProperty({through: START_DATE, resolve: true})
+  startDate: string;
+  @subjectPropertySetter({resolveLanguage: 'literal'})
+  setStartDate(startDate: string) {}
+
+  @subjectProperty({through: END_DATE, resolve: true})
+  endDate: string;
+  @subjectPropertySetter({resolveLanguage: 'literal'})
+  setEndDate(endDate: string) {}
+
+  @subjectProperty({through: URL, resolve: true})
+  url: string;
+  @subjectPropertySetter({resolveLanguage: 'literal'})
+  setUrl(endDate: string) {}
+
+  @subjectCollection({through: EntryType.Message})
+  comments: string[];
+  addComment(comment: string) {}
 }
 
-export default PostModel;
+export default Post;
