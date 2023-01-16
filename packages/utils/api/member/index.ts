3fcfa166c0e0fc90959c4af3ae3c8b1302a9eccd
Nicolas Luck <nicolas@lucksus.eu>
2023-01-16 18:21:00 +0000

    Migrate MemberModel to Subject class

--- packages/utils/api/member/index.ts
+++ packages/utils/api/member/index.ts
@@ -1,42 +1,9 @@
-import {
-  CHANNEL_VIEW,
-  DID,
-  MEMBER,
-  NAME,
-} from "../../constants/communityPredicates";
-import EntryModel from "../../helpers/model";
-import { EntryType, Entry, ChannelView } from "../../types";
+import { sdnaOutput } from "@perspect3vism/ad4m";
+class Member {
+  isSubjectInstance = ['languageName(this, "did")']
 
-export interface Member extends Entry {
-  did: string;
+  @sdnaOutput
+  static generateSdna(): string { return "" }
 }
 
-class MemberModel extends EntryModel {
-  static get type() {
-    return EntryType.Member;
-  }
-
-  static get properties() {
-    return {
-      did: {
-        predicate: DID,
-        type: String,
-        resolve: false,
-      },
-    };
-  }
-
-  async create(data: { did: string }): Promise<Member> {
-    return super.create(data) as Promise<Member>;
-  }
-
-  async getAll() {
-    return super.getAll() as Promise<Member[]>;
-  }
-
-  async get(id: string) {
-    return super.get(id) as Promise<Member>;
-  }
-}
-
-export default MemberModel;
+export default Member;
