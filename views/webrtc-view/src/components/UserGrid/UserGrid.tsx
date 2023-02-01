d29ff6f982ca40d13695dfdadfecb29f8724fa9f
Bård N. Hovde <bnhovde@gmail.com>
2023-02-01 14:05:39 +0100

    Correct grid sizing for users

--- views/webrtc-view/src/components/UserGrid/UserGrid.tsx
+++ views/webrtc-view/src/components/UserGrid/UserGrid.tsx
@@ -12,6 +12,17 @@ export default function UserGrid() {
   } = useContext(WebRTCContext);
 
   const cameraEnabled = currentUser && currentUser.prefrences.video;
+  const userCount = participants.length + (currentUser ? 1 : 0);
+
+  // Grid sizing
+  let gridCol = userCount === 1 ? 1 : userCount <= 4 ? 2 : 4;
+  const gridColSize = userCount <= 4 ? 1 : 2;
+  let gridRowSize = userCount <= 4 ? userCount : Math.ceil(userCount / 2);
+
+  // if (screenPresenter) {
+  //   gridCol = 1;
+  //   gridRowSize = 2;
+  // }
 
   useEffect(() => {
     if (videoRef.current) {
@@ -21,7 +32,14 @@ export default function UserGrid() {
   }, [videoRef, currentUser, stream]);
 
   return (
-    <section className={styles.grid}>
+    <section
+      className={styles.grid}
+      style={{
+        "--grid-size": gridCol,
+        "--grid-col-size": gridColSize,
+        "--grid-row-size": gridRowSize,
+      }}
+    >
       {currentUser && (
         <Item
           data={currentUser}
