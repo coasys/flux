7dfc1fedfd82fc18641c08b25c6d2a95a8931496
Bård N. Hovde <bnhovde@gmail.com>
2023-02-07 09:45:26 +0100

    more STUN servers

--- views/webrtc-view/src/WebRTCManager.ts
+++ views/webrtc-view/src/WebRTCManager.ts
@@ -10,7 +10,12 @@ import {
 const servers = {
   iceServers: [
     {
-      urls: ["stun:stun.services.mozilla.com"],
+      urls: [
+        "stun:stun.l.google.com:19302",
+        "stun:stun1.l.google.com:19302",
+        "stun:stun2.l.google.com:19302",
+        "stun:stun.services.mozilla.com",
+      ],
     },
   ],
   iceCandidatePoolSize: 10,
