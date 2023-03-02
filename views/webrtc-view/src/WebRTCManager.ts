afa321eb2f74f81b295533d82b75d073feb17c3a
Bård N. Hovde <bnhovde@gmail.com>
2023-03-02 17:32:29 +0100

    Removed dead server

--- views/webrtc-view/src/WebRTCManager.ts
+++ views/webrtc-view/src/WebRTCManager.ts
@@ -17,7 +17,6 @@ const servers = {
         "stun:stun.l.google.com:19302",
         "stun:stun1.l.google.com:19302",
         "stun:stun2.l.google.com:19302",
-        "stun:stun.services.mozilla.com",
       ],
     },
     {
