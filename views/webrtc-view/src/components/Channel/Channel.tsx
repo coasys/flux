c8b872d6a1cb3e9b4a62dc97ce01f00b3305ad64
Bård N. Hovde <bnhovde@gmail.com>
2023-03-07 15:02:50 +0100

    Reworked join screen, closes #385

--- views/webrtc-view/src/components/Channel/Channel.tsx
+++ views/webrtc-view/src/components/Channel/Channel.tsx
@@ -1,17 +1,16 @@
 import { useContext, useEffect, useRef, useState } from "preact/hooks";
 import useWebRTC from "../../hooks/useWebrtc";
+import useIntersectionObserver from "../../hooks/useIntersectionObserver";
 import getMe, { Me } from "utils/api/getMe";
 
 import UserGrid from "../UserGrid";
 import Footer from "../Footer";
-import Debug from "../Debug";
-
-import styles from "./Channel.module.css";
 import Notifications from "../Notifications";
 import UiContext from "../../context/UiContext";
-import Disclaimer from "../Disclaimer";
 import Overlay from "../Overlay/Overlay";
-import useIntersectionObserver from "../../hooks/useIntersectionObserver";
+import JoinScreen from "../JoinScreen";
+
+import styles from "./Channel.module.css";
 
 export default function Channel({ source, uuid }) {
   const [agent, setAgent] = useState<Me | null>(null);
@@ -50,63 +49,21 @@ export default function Channel({ source, uuid }) {
   return (
     <section className={styles.outer} ref={wrapperEl}>
       {!webRTC.hasJoined && (
-        <div className={styles.join}>
-          <j-flex a="center" direction="column">
-            <h1>You haven't joined this room</h1>
-
-            <j-text variant="body">Your microphone will be enabled.</j-text>
-
-            <j-box pt="200">
-              <Disclaimer />
-            </j-box>
-
-            <j-box pt="400">
-              <j-toggle
-                checked={webRTC.settings.video}
-                disabled={webRTC.isLoading || !webRTC.permissionGranted}
-                onChange={() =>
-                  webRTC.onChangeSettings({
-                    ...webRTC.settings,
-                    video: !webRTC.settings.video,
-                  })
-                }
-              >
-                Join with camera!
-              </j-toggle>
-            </j-box>
-
-            <j-box pt="500">
-              <j-button
-                variant="primary"
-                size="lg"
-                loading={webRTC.isLoading}
-                disabled={!webRTC.permissionGranted}
-                onClick={webRTC.onJoin}
-              >
-                Join room!
-              </j-button>
-            </j-box>
-
-            <>
-              {!webRTC.permissionGranted && (
-                <j-box pt="400">
-                  <j-text variant="warning">
-                    Please allow camera/microphone access to join.
-                  </j-text>
-                </j-box>
-              )}
-            </>
-          </j-flex>
-        </div>
-      )}
-
-      <UserGrid webRTC={webRTC} currentUser={agent} />
-
-      {webRTC.hasJoined && (
-        <Footer
+        <JoinScreen
           webRTC={webRTC}
+          currentUser={agent}
           onToggleSettings={() => toggleShowSettings(!showSettings)}
         />
+      )}
+
+      {webRTC.hasJoined && (
+        <>
+          <UserGrid webRTC={webRTC} currentUser={agent} />
+          <Footer
+            webRTC={webRTC}
+            onToggleSettings={() => toggleShowSettings(!showSettings)}
+          />
+        </>
       )}
 
       <Overlay webRTC={webRTC} currentUser={agent} />
