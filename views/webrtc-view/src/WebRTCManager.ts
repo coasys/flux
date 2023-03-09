f80a324cefb718d5265820ff7542224de23bc675
Bård N. Hovde <bnhovde@gmail.com>
2023-03-09 19:58:59 +0100

    Restructured and commented WebRTCManager

--- views/webrtc-view/src/WebRTCManager.ts
+++ views/webrtc-view/src/WebRTCManager.ts
@@ -167,6 +167,9 @@ export default class WebRTCManager {
     this.callbacks[event].push(cb);
   }
 
+  /**
+   * Handle incoming signals
+   */
   async onSignal(expression: PerspectiveExpression) {
     if (!this.isListening) return;
 
@@ -184,31 +187,31 @@ export default class WebRTCManager {
     if (!link) return;
 
     if (link.data.predicate === LEAVE && link.data.source === this.roomId) {
-      this.closeConnection(link.author);
+      await this.closeConnection(link.author);
     }
 
     if (
       link.data.predicate === OFFER_REQUEST &&
       link.data.source === this.roomId
     ) {
-      this.createOffer(link.author);
+      await this.createOffer(link.author);
     }
 
     // If we get heartbeat from new user, action!
     if (link.data.predicate === HEARTBEAT && link.data.source === this.roomId) {
       if (!this.connections.get(link.author)) {
-        this.createOffer(link.author);
+        await this.createOffer(link.author);
       }
     }
     // Only handle the offer if it's for me
     if (link.data.predicate === OFFER && link.data.source === this.agent.did) {
       const offer = Literal.fromUrl(link.data.target).get();
-      this.handleOffer(link.author, offer);
+      await this.handleOffer(link.author, offer);
     }
     // Only handle the answer if it's for me
     if (link.data.predicate === ANSWER && link.data.source === this.agent.did) {
       const answer = Literal.fromUrl(link.data.target).get();
-      this.handleAnswer(link.author, answer);
+      await this.handleAnswer(link.author, answer);
     }
     // Only handle the answer if it's for me
     if (
@@ -216,36 +219,56 @@ export default class WebRTCManager {
       link.data.source === this.agent.did
     ) {
       const candidate = Literal.fromUrl(link.data.target).get();
-      this.handleIceCandidate(link.author, candidate);
+      await this.handleIceCandidate(link.author, candidate);
     }
 
     return null;
   }
 
-  async handleIceCandidate(fromDid: string, candidate: RTCIceCandidate) {
-    const connection = this.connections.get(fromDid);
-    // Make sure we have a remote description;
-    if (connection && !connection.peerConnection.currentRemoteDescription) {
-      console.log(
-        "🔴 Skipping ICE candidate adding as currentRemoteDescription is ",
-        connection.peerConnection.currentRemoteDescription
-      );
+  /**
+   * Create Offer - Entry point for new connections
+   *
+   * 1: Create new connection, add to connections
+   * 2: Add local audio/video tracks to peerconnection
+   * 3: Create offer and set local description
+   * 5: Broadcast offer to remote peer
+   */
+  async createOffer(recieverDid: string) {
+    // Don't create an offer if we alread have a connection
+    if (this.connections.get(recieverDid)) {
+      this.closeConnection(recieverDid);
     }
-    if (connection && connection.peerConnection.currentRemoteDescription) {
-      connection.peerConnection.addIceCandidate(new RTCIceCandidate(candidate));
-    }
+
+    const connection = await this.addConnection(recieverDid);
+
+    this.localStream.getTracks().forEach((track) => {
+      connection.peerConnection.addTrack(track, this.localStream);
+    });
+
+    const offer = await connection.peerConnection.createOffer();
+    await connection.peerConnection.setLocalDescription(offer);
+
+    console.log("🟠 Sending OFFER signal to ", recieverDid);
+    this.neighbourhood.sendBroadcastU({
+      links: [
+        {
+          source: recieverDid,
+          predicate: OFFER,
+          target: Literal.from(offer).toUrl(),
+        },
+      ],
+    });
   }
 
-  async closeConnection(did: string) {
-    const connection = this.connections.get(did);
-
-    if (connection) {
-      connection.peerConnection.close();
-      connection.dataChannel.close();
-      this.connections.delete(did);
-    }
-  }
-
+  /**
+   * Create connection and add to connections array
+   *
+   * 1: Establish new RTC connection and datachannel
+   * 2: Listen for ICE candidates on peer connection
+   *    - Pass these to the remote peer via broadcast
+   * 3: Listen for connection state change
+   *    - If disconnected, delete connection
+   */
   async addConnection(remoteDid: string) {
     if (this.connections.get(remoteDid)) {
       return this.connections.get(remoteDid);
@@ -274,13 +297,6 @@ export default class WebRTCManager {
       });
     };
 
-    const newConnection = {
-      peerConnection,
-      dataChannel,
-    };
-
-    this.connections.set(remoteDid, newConnection);
-
     peerConnection.addEventListener("icecandidate", async (event) => {
       if (event.candidate) {
         console.log("🟠 Sending ICE_CANDIDATE signal to ", remoteDid);
@@ -308,36 +324,25 @@ export default class WebRTCManager {
       });
     });
 
+    const newConnection = {
+      peerConnection,
+      dataChannel,
+    };
+
+    this.connections.set(remoteDid, newConnection);
+
     return newConnection;
   }
 
-  async createOffer(recieverDid: string) {
-    // Don't create an offer if we alread have a connection
-    if (this.connections.get(recieverDid)) {
-      this.closeConnection(recieverDid);
-    }
-
-    const connection = await this.addConnection(recieverDid);
-
-    this.localStream.getTracks().forEach((track) => {
-      connection.peerConnection.addTrack(track, this.localStream);
-    });
-
-    const offer = await connection.peerConnection.createOffer();
-    await connection.peerConnection.setLocalDescription(offer);
-
-    console.log("🟠 Sending OFFER signal to ", recieverDid);
-    this.neighbourhood.sendBroadcastU({
-      links: [
-        {
-          source: recieverDid,
-          predicate: OFFER,
-          target: Literal.from(offer).toUrl(),
-        },
-      ],
-    });
-  }
-
+  /**
+   * Process offer from peer, create answer
+   *
+   * 1: Add peer to local connections
+   * 2: Add offer to remote description
+   * 3: Add local audio/video tracks to peerconnection
+   * 4: Create answer and add to local description
+   * 5: Broadcast answer to remote peer
+   */
   async handleOffer(fromDid: string, offer: RTCSessionDescriptionInit) {
     // Don't create an answer if we alread have a connection
     if (this.connections.get(fromDid)) return;
@@ -367,6 +372,12 @@ export default class WebRTCManager {
     });
   }
 
+  /**
+   * Process answer from peer
+   *
+   * 1: Check that peerConnection has remote description
+   * 2: Set remote description to answer
+   */
   async handleAnswer(fromDid: string, answer: RTCSessionDescriptionInit) {
     const connection = this.connections.get(fromDid);
     if (connection && !connection.peerConnection.currentRemoteDescription) {
@@ -377,6 +388,29 @@ export default class WebRTCManager {
     }
   }
 
+  /**
+   * Process ICE candidate from peer
+   *
+   * 1: Check that peerConnection does not have a remote description
+   * 2: Add ice candidate to peer connection
+   */
+  async handleIceCandidate(fromDid: string, candidate: RTCIceCandidate) {
+    const connection = this.connections.get(fromDid);
+    // Make sure we have a remote description;
+    if (connection && !connection.peerConnection.currentRemoteDescription) {
+      console.log(
+        "🔴 Skipping ICE candidate adding as currentRemoteDescription is ",
+        connection.peerConnection.currentRemoteDescription
+      );
+    }
+    if (connection && connection.peerConnection.currentRemoteDescription) {
+      connection.peerConnection.addIceCandidate(new RTCIceCandidate(candidate));
+    }
+  }
+
+  /**
+   * Send message via datachannel
+   */
   async sendMessage(type: string, message: any, recepients?: string[]) {
     const data = JSON.stringify({
       type,
@@ -407,6 +441,22 @@ export default class WebRTCManager {
     });
   }
 
+  /**
+   * Close connection/datachannel and remove from connections array
+   */
+  async closeConnection(did: string) {
+    const connection = this.connections.get(did);
+
+    if (connection) {
+      connection.peerConnection.close();
+      connection.dataChannel.close();
+      this.connections.delete(did);
+    }
+  }
+
+  /**
+   * Join the chat room, listen for signals and begin heartbeat
+   */
   async join(initialSettings?: Settings) {
     console.log("Start joining");
 
@@ -438,6 +488,40 @@ export default class WebRTCManager {
     this.heartbeatId = setInterval(this.heartbeat, 10000);
 
     return this.localStream;
+  }
+
+  /**
+   * Leave the room and close all connections
+   */
+  async leave() {
+    this.isListening = false;
+
+    // Stop heartbeat
+    clearInterval(this.heartbeatId);
+
+    if (this.perspective) {
+      // Todo: Nico, nico, nico!
+      // this.neighbourhood.
+    }
+
+    // Announce departure
+    this.neighbourhood.sendBroadcastU({
+      links: [
+        {
+          source: this.roomId,
+          predicate: LEAVE,
+          target: "goodbye!", // could be empty
+        },
+      ],
+    });
+
+    // Close webrtc connections
+    this.connections.forEach((c, key) => {
+      this.closeConnection(key);
+    });
+
+    // Kill media recording
+    this.localStream.getTracks().forEach((track) => track.stop());
   }
 
   async heartbeat() {
@@ -477,36 +561,5 @@ export default class WebRTCManager {
         },
       ],
     });
-  }
-
-  async leave() {
-    this.isListening = false;
-
-    // Stop heartbeat
-    clearInterval(this.heartbeatId);
-
-    if (this.perspective) {
-      // Todo: Nico, nico, nico!
-      // this.neighbourhood.
-    }
-
-    // Announce departure
-    this.neighbourhood.sendBroadcastU({
-      links: [
-        {
-          source: this.roomId,
-          predicate: LEAVE,
-          target: "goodbye!", // could be empty
-        },
-      ],
-    });
-
-    // Close webrtc connections
-    this.connections.forEach((c, key) => {
-      this.closeConnection(key);
-    });
-
-    // Kill media recording
-    this.localStream.getTracks().forEach((track) => track.stop());
   }
 }
