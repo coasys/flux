a94ca0452bc92aaa437b18739f31c120091c1089
Bård N. Hovde <bnhovde@gmail.com>
2023-04-04 16:22:03 +0200

    Fixed offer request loop

--- packages/utils/helpers/WebRTCManager.ts
+++ packages/utils/helpers/WebRTCManager.ts
@@ -232,9 +232,9 @@ export default class WebRTCManager {
         // Check if we should create the offer or not
         if (link.author.localeCompare(this.agent.did) > 0) {
           await this.createOffer(link.author);
+        } else {
+          this.broadcastOfferRequest();
         }
-      } else {
-        this.broadcastOfferRequest();
       }
 
       this.addToEventLog(link.author, link?.data?.predicate || "unknown");
