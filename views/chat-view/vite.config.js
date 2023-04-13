e46884d101a649395d9fa86ba250f075b3ee7684
Fayeed <fayeed@live.com>
2023-04-13 14:46:14 +0530

    Vite config updated for chat-view

--- views/chat-view/vite.config.js
+++ views/chat-view/vite.config.js
@@ -4,7 +4,14 @@ import preact from "@preact/preset-vite";
 import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 
 export default defineConfig({
-  plugins: [preact(), cssInjectedByJsPlugin()],
+  plugins: [preact({
+    babel: {
+      plugins: [
+        ["@babel/plugin-proposal-decorators", { legacy: true }],
+        ["@babel/plugin-proposal-class-properties"],
+      ],
+    },
+  }), cssInjectedByJsPlugin()],
   build: {
     lib: {
       // Could also be a dictionary or array of multiple entry points
