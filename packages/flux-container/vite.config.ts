12d2c1c810b70b6c01b28799a322de71c01d77f6
Bård N. Hovde <bnhovde@gmail.com>
2023-05-03 21:14:17 +0200

    Add build analyzer

--- packages/flux-container/vite.config.ts
+++ packages/flux-container/vite.config.ts
@@ -1,5 +1,6 @@
-import { defineConfig } from "vite";
+import { PluginOption, defineConfig } from "vite";
 import { resolve } from "path";
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -17,4 +18,13 @@ export default defineConfig({
       },
     },
   },
+  plugins: [
+    visualizer({
+      template: "treemap", // or sunburst
+      open: true,
+      gzipSize: true,
+      brotliSize: true,
+      filename: "analice.html",
+    }) as PluginOption,
+  ],
 });
