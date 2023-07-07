7afa553f7b11d0167a69493797d513c1aa84ba53
Leif Riksheim <riksheim.leif@gmail.com>
2023-07-07 10:44:26 +0100

    check if we have tasks already, use correct columnId

--- views/kanban-view/src/components/Board.tsx
+++ views/kanban-view/src/components/Board.tsx
@@ -28,26 +28,34 @@ export default function Board({ perspective, source }: BoardProps) {
   const [tasks, setTasks] = useState([]);
 
   useEffect(() => {
-    perspective.addSdna(`
-    subject_class("Task", pvwrkd).
-    constructor(pvwrkd, '[{action: "addLink", source: "this", predicate: "rdf://name", target: "literal://json:%7B%22author%22%3A%22did%3Akey%3AzQ3shWhAZydqDE1s5Ka3gJTDpjdYg1Ajw6PFywSP7QJD8x8ki%22%2C%22timestamp%22%3A%222023-07-06T16%3A25%3A41.597Z%22%2C%22data%22%3A%22New%20task%22%2C%22proof%22%3A%7B%22key%22%3A%22%23zQ3shWhAZydqDE1s5Ka3gJTDpjdYg1Ajw6PFywSP7QJD8x8ki%22%2C%22signature%22%3A%2281a4f1b56d73e3069cd0a35addc31bd418c2d15a336110de3c1c8639bcc01c857a888846e5cd0714f6a0457bcef609a7cd23ecb578dcf00a25f848951013de3d%22%2C%22valid%22%3Atrue%2C%22invalid%22%3Afalse%7D%7D"},{action: "addLink", source: "this", predicate: "rdf://status", target: "literal://json:%7B%22author%22%3A%22did%3Akey%3AzQ3shWhAZydqDE1s5Ka3gJTDpjdYg1Ajw6PFywSP7QJD8x8ki%22%2C%22timestamp%22%3A%222023-07-06T16%3A25%3A41.606Z%22%2C%22data%22%3A%22todo%22%2C%22proof%22%3A%7B%22key%22%3A%22%23zQ3shWhAZydqDE1s5Ka3gJTDpjdYg1Ajw6PFywSP7QJD8x8ki%22%2C%22signature%22%3A%226ca7bb8bc2db7d90b995902f6278974a879eeafe871885614933cc553f1afe935cc4e0221c7de7e7b2c46d1d6dbc4c752879a782e24cd134afaa63812297a05e%22%2C%22valid%22%3Atrue%2C%22invalid%22%3Afalse%7D%7D"}]').
-    instance(pvwrkd, Base) :- triple(Base, "rdf://name", _),triple(Base, "rdf://status", _).
-    
-    property(pvwrkd, "name").
-    property_resolve(pvwrkd, "name").
-    property_resolve_language(pvwrkd, "name", "literal").
-    property_getter(pvwrkd, Base, "name", Value) :- triple(Base, "rdf://name", Value).
-    property_setter(pvwrkd, "name", '[{action: "setSingleTarget", source: "this", predicate: "rdf://name", target: "value"}]').
-        
-    property(pvwrkd, "status").
-    property_resolve(pvwrkd, "status").
-    property_resolve_language(pvwrkd, "status", "literal").
-    property_getter(pvwrkd, Base, "status", Value) :- triple(Base, "rdf://status", Value).
-    property_setter(pvwrkd, "status", '[{action: "setSingleTarget", source: "this", predicate: "rdf://status", target: "value"}]').
-    property_named_option(pvwrkd, "status", "todo", "todo").
-    property_named_option(pvwrkd, "status", "doing", "doing").
-    property_named_option(pvwrkd, "status", "done", "done")
-    `);
+    perspective.infer(`subject_class("Task", Atom)`).then((hasTask) => {
+      if (!hasTask) {
+        perspective
+          .addSdna(
+            `
+              subject_class("Task", pvwrkd).
+              constructor(pvwrkd, '[{action: "addLink", source: "this", predicate: "rdf://name", target: "literal://json:%7B%22author%22%3A%22did%3Akey%3AzQ3shWhAZydqDE1s5Ka3gJTDpjdYg1Ajw6PFywSP7QJD8x8ki%22%2C%22timestamp%22%3A%222023-07-06T16%3A25%3A41.597Z%22%2C%22data%22%3A%22New%20task%22%2C%22proof%22%3A%7B%22key%22%3A%22%23zQ3shWhAZydqDE1s5Ka3gJTDpjdYg1Ajw6PFywSP7QJD8x8ki%22%2C%22signature%22%3A%2281a4f1b56d73e3069cd0a35addc31bd418c2d15a336110de3c1c8639bcc01c857a888846e5cd0714f6a0457bcef609a7cd23ecb578dcf00a25f848951013de3d%22%2C%22valid%22%3Atrue%2C%22invalid%22%3Afalse%7D%7D"},{action: "addLink", source: "this", predicate: "rdf://status", target: "literal://json:%7B%22author%22%3A%22did%3Akey%3AzQ3shWhAZydqDE1s5Ka3gJTDpjdYg1Ajw6PFywSP7QJD8x8ki%22%2C%22timestamp%22%3A%222023-07-06T16%3A25%3A41.606Z%22%2C%22data%22%3A%22todo%22%2C%22proof%22%3A%7B%22key%22%3A%22%23zQ3shWhAZydqDE1s5Ka3gJTDpjdYg1Ajw6PFywSP7QJD8x8ki%22%2C%22signature%22%3A%226ca7bb8bc2db7d90b995902f6278974a879eeafe871885614933cc553f1afe935cc4e0221c7de7e7b2c46d1d6dbc4c752879a782e24cd134afaa63812297a05e%22%2C%22valid%22%3Atrue%2C%22invalid%22%3Afalse%7D%7D"}]').
+              instance(pvwrkd, Base) :- triple(Base, "rdf://name", _),triple(Base, "rdf://status", _).
+              
+              property(pvwrkd, "name").
+              property_resolve(pvwrkd, "name").
+              property_resolve_language(pvwrkd, "name", "literal").
+              property_getter(pvwrkd, Base, "name", Value) :- triple(Base, "rdf://name", Value).
+              property_setter(pvwrkd, "name", '[{action: "setSingleTarget", source: "this", predicate: "rdf://name", target: "value"}]').
+                  
+              property(pvwrkd, "status").
+              property_resolve(pvwrkd, "status").
+              property_resolve_language(pvwrkd, "status", "literal").
+              property_getter(pvwrkd, Base, "status", Value) :- triple(Base, "rdf://status", Value).
+              property_setter(pvwrkd, "status", '[{action: "setSingleTarget", source: "this", predicate: "rdf://status", target: "value"}]').
+              property_named_option(pvwrkd, "status", "todo", "todo").
+              property_named_option(pvwrkd, "status", "doing", "doing").
+              property_named_option(pvwrkd, "status", "done", "done").
+            `
+          )
+          .then(() => getClasses(perspective, source).then(setClasses));
+      }
+    });
   }, []);
 
   const { entries, model } = useEntries({
@@ -58,6 +66,7 @@ export default function Board({ perspective, source }: BoardProps) {
 
   function loadColumns() {
     getNamedOptions(perspective, selectedClass).then((namedOpts) => {
+      console.log({ namedOptions, selectedClass });
       setNamedOptions(namedOpts);
       Object.keys(namedOpts).forEach((key, index) => {
         if (index === 0) {
@@ -73,7 +82,7 @@ export default function Board({ perspective, source }: BoardProps) {
 
   useEffect(() => {
     getClasses(perspective, source).then(setClasses);
-  }, [perspective.uuid]);
+  }, [perspective.uuid, selectedClass]);
 
   useEffect(() => {
     loadColumns();
@@ -87,8 +96,8 @@ export default function Board({ perspective, source }: BoardProps) {
     );
   }, [tasks, selectedProperty, perspective.uuid, namedOptions]);
 
-  function createNewTodo() {
-    model.create({});
+  function createNewTodo(propertyName, value) {
+    model.create({ [propertyName]: value });
   }
 
   const onDragEnd = (result) => {
@@ -111,6 +120,7 @@ export default function Board({ perspective, source }: BoardProps) {
     setTasks((oldTasks) => {
       return oldTasks.map((t) => (t.id === draggableId ? { ...t, status } : t));
     });
+    console.log({ destination, draggableId });
     model.update(draggableId, { status });
   };
 
@@ -168,7 +178,7 @@ export default function Board({ perspective, source }: BoardProps) {
                   <j-text nomargin variant="heading-sm">
                     {column.title}
                   </j-text>
-                  <Droppable droppableId={index.toString()}>
+                  <Droppable droppableId={columnId}>
                     {(provided, snapshot) => (
                       <div
                         ref={provided.innerRef}
@@ -202,7 +212,10 @@ export default function Board({ perspective, source }: BoardProps) {
                       </div>
                     )}
                   </Droppable>
-                  <j-button variant="link" onClick={createNewTodo}>
+                  <j-button
+                    variant="link"
+                    onClick={() => createNewTodo(selectedProperty, columnId)}
+                  >
                     Add card
                     <j-icon name="plus" size="md" slot="start"></j-icon>
                   </j-button>
