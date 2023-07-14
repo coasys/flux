cf200ef1531f48597eb83272cb065335e3c53b64
Fayeed <fayeed@live.com>
2023-07-14 12:08:28 +0530

    Added a new Avatar component to chat-view that handles the profile image fetching

---
+++ views/chat-view/src/components/Avatar/Avatar.tsx
@@ -0,0 +1,40 @@
+import { getImage } from "@fluxapp/utils";
+import { useEffect, useState } from "preact/hooks";
+
+type AvatarProps = {
+  size: "" | "xxs" | "xs" | "md" | "sm" | "lg" | "xl" | "xxl";
+  profileAddress?: string;
+  hash?: string;
+}
+
+export default function Avatar({size = 'md', hash, profileAddress}: AvatarProps) {
+  const [image, setImage] = useState<string>();
+
+  useEffect(() => {
+    if (typeof profileAddress === "string") {
+      if (profileAddress.includes("base64")) {
+        setImage(profileAddress);
+      } else {
+        getProfileImage(profileAddress);
+      }
+    } else {
+      setImage(null)
+    }
+  }, [profileAddress])
+
+  async function getProfileImage(url: string) {
+    try {
+      const src = await getImage(url);
+      setImage(src)
+    } finally {
+    }
+  }
+
+  return (
+    <j-avatar
+      size={size}
+      src={image}
+      hash={hash}
+    ></j-avatar>
+  )
+}
