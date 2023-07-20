2869018442170ed0b1199996b8c5eee8ae80632f
Leif Riksheim <riksheim.leif@gmail.com>
2023-07-20 13:03:55 +0200

    Add appUrl

--- app/src/ad4mConnect.ts
+++ app/src/ad4mConnect.ts
@@ -3,6 +3,7 @@ import Ad4mConnectUI from "@perspect3vism/ad4m-connect";
 const ad4mConnect = Ad4mConnectUI({
   appName: "Flux",
   appDesc: "A Social Toolkit for the New Internet",
+  appUrl: window.location.origin,
   appDomain: window.location.origin,
   appIconPath: "https://i.ibb.co/GnqjPJP/icon.png",
   capabilities: [{ with: { domain: "*", pointers: ["*"] }, can: ["*"] }],
