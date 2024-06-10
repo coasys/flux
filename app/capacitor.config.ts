cb64c43f739f4388feb3c2ecf00a966408e45ea4
Fayeed Pawaskar <fayeed@live.com>
2024-06-10 10:32:01 +0530

    chore: Add Android and iOS project files and configurations

---
+++ app/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.coasys.flux',
+  appName: 'flux',
+  webDir: 'dist'
+};
+
+export default config;
