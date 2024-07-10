74a7cb0efff1b1e7dc121560c7d50876fced264a
Fayeed Pawaskar <fayeed@live.com>
2024-07-10 12:13:24 +0530

    chore: Update dependencies and project structure for Capacitor plugins

--- app/capacitor.config.ts
+++ app/capacitor.config.ts
@@ -3,7 +3,13 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.coasys.flux',
   appName: 'flux',
-  webDir: 'dist'
+  webDir: 'dist',
+  plugins: {
+    "StatusBar": {
+      "style": "DARK",
+      "backgroundColor": "rgb(20, 18, 22)"
+    }
+  }
 };
 
 export default config;
