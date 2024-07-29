ede961484534f183635dc879d0bcfad8bede4f6e
jhweir <james@weco.io>
2024-07-29 11:47:44 +0100

    Xenova whisper-tiny & whisper-base models bundled in the webrtc view app

--- views/webrtc-view/src/components/Transcriber/worker.ts
+++ views/webrtc-view/src/components/Transcriber/worker.ts
@@ -1,15 +1,13 @@
-import { pipeline } from "@xenova/transformers";
+import { env, pipeline } from "@xenova/transformers";
+env.allowLocalModels = true;
+env.localModelPath = "/models/";
 
 onmessage = async function (message) {
   const { id, float32Array, model } = message.data;
-  const pipe = await pipeline(
-    "automatic-speech-recognition",
-    `${model}${model === "distil-whisper/distil-large-v2" ? "" : ".en"}`,
-    {
-      quantized: true,
-      revision: model.includes("whisper-medium") ? "no_attentions" : "main",
-    }
-  );
+  const pipe = (await pipeline("automatic-speech-recognition", model, {
+    quantized: true,
+    // revision: model.includes("whisper-medium") ? "no_attentions" : "main",
+  })) as any;
   const transcription = (await pipe(float32Array, {
     chunk_length_s: 30,
   })) as any;
