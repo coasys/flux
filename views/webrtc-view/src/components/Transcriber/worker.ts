85c9e83ea745a5ab25ec00abda533f9da8eb89d6
Fayeed Pawaskar <fayeed@live.com>
2024-08-14 12:55:35 +0530

    chore: Update worker.ts to include cos_sim import and remove unused code

--- views/webrtc-view/src/components/Transcriber/worker.ts
+++ views/webrtc-view/src/components/Transcriber/worker.ts
@@ -1,4 +1,4 @@
-import { pipeline } from "@xenova/transformers";
+import { pipeline, cos_sim } from "@xenova/transformers";
 
 let pipe;
 let currentModel = "";
@@ -30,52 +30,11 @@ async function transcribe() {
     transcriptionQueue.shift();
     if (transcriptionQueue.length) transcribe();
     else transcribing = false;
-    postMessage({ id, text: result.text, type: "transcribe" });
+    postMessage({ id, text: result.text });
   });
 }
 
 onmessage = async function (message) {
-  if (message.data.type === "transcribe") {
-    transcriptionQueue.push(message.data);
-    if (!transcribing) transcribe();
-  } else if (message.data.type === "similarity") {
-    const { text } = message.data;
-    const data = await embed(text);
-    postMessage({ type: "similarity", text, embedding: data });
-  }
+  transcriptionQueue.push(message.data);
+  if (!transcribing) transcribe();
 };
-
-
-
-async function embed(text: string, embedNewText=true) {
-  if (!embedder) {
-      embedder = await pipeline("feature-extraction", "TaylorAI/gte-tiny", {
-          quantized: true,
-      });
-  }
-
-  if (text in embeddingsDict) {
-      return embeddingsDict[text];
-  }
-
-  if (embedNewText==false){
-      if (currentNullVector.length !== 0){
-          embeddingsDict[text] = currentNullVector;
-          return currentNullVector
-      }
-      else {
-          const tempVec = await embedder("test", { pooling: 'mean', normalize: true });
-          currentNullVector = [...tempVec.data].fill(0.00001);
-          embeddingsDict[text] = currentNullVector;
-          return currentNullVector
-      }
-  }
-
-  const e0 = await embedder(text, { pooling: 'mean', normalize: true });
-
-  const roundDecimalsDown = (num) => parseFloat(num.toFixed(3));
-
-  embeddingsDict[text] = e0.data.map(roundDecimalsDown);
-
-  return e0.data;
-}
