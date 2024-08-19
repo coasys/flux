724158308132b2c547bc6f8f73b666091083db9d
Fayeed Pawaskar <fayeed@live.com>
2024-08-19 12:03:30 +0530

    Refactor languages.ts to add embedding vector language constant

--- packages/constants/src/languages.ts
+++ packages/constants/src/languages.ts
@@ -1,2 +1,5 @@
 export const FILE_STORAGE_LANGUAGE =
   "QmzSYwdjqeP9D13Sfmyc5HcabM9jL3DtPyhadnF6dQXu4FjVSbQ";
+
+export const EMBEDDING_VECTOR_LANGUAGE = 
+  "QmzSYwdcW2siKPjxdzpLZZKmQhZKiHPbwCJiBPKg31pMi73UmFz";
