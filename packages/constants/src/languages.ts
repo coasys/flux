37f2da929a0a130be7f507d1e9e167a3e5462adf
Fayeed Pawaskar <fayeed@live.com>
2024-08-19 13:40:18 +0530

    Refactor languages.ts to update EMBEDDING_VECTOR_LANGUAGE constant

--- packages/constants/src/languages.ts
+++ packages/constants/src/languages.ts
@@ -2,4 +2,4 @@ export const FILE_STORAGE_LANGUAGE =
   "QmzSYwdjqeP9D13Sfmyc5HcabM9jL3DtPyhadnF6dQXu4FjVSbQ";
 
 export const EMBEDDING_VECTOR_LANGUAGE = 
-  "QmzSYwdcW2siKPjxdzpLZZKmQhZKiHPbwCJiBPKg31pMi73UmFz";
+  "QmzSYwdiqjYXRAaoJdARpP7xRj4VQfdTT3J4HNGLohdKeuBgo1E";
