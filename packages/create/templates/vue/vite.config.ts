33b39b7ddf0e49207cafa0de92ee5a18fe03b054
Nicolas Luck <nicolas@lucksus.eu>
2024-12-19 00:04:28 +0100

    Update ad4m deps to 0.10.0-rc11 and rename and align flux packages to the same version string

--- packages/create/templates/vue/vite.config.ts
+++ packages/create/templates/vue/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 import vue from '@vitejs/plugin-vue'
 import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
+import babel from 'vite-plugin-babel-compiler';
 
 export default defineConfig({
   plugins: [
@@ -10,6 +11,14 @@ export default defineConfig({
         compilerOptions: {
           isCustomElement: tag => tag.startsWith('j-')
         }
+      },
+    }),
+    babel({
+      babelConfig: {
+        plugins: [
+          ["@babel/plugin-proposal-decorators", { "legacy": true }],
+          ["@babel/plugin-proposal-class-properties", { "loose": true }]
+        ]
       }
     }),
     cssInjectedByJsPlugin(),
