70af34fe19a99d2e6774a2f092005def2f835e46
jhweir <james@weco.io>
2025-07-14 19:52:04 +0100

    Start on pinned & recent conversation blocks in sidebar

---
+++ app/src/views/main/community/community-sidebar/PinnedConversations.vue
@@ -0,0 +1,43 @@
+<template>
+  <j-box mt="800" mb="400" px="500">
+    <j-flex direction="column" gap="400">
+      <j-flex a="center" gap="300">
+        <j-icon name="pin" size="sm" color="ui-400" />
+        <j-text color="ui-400" uppercase nomargin>Pinned Conversations</j-text>
+      </j-flex>
+
+      <j-spinner size="sm" v-if="!pinnedConversations.length && pinnedConversationsLoading" />
+
+      <div
+        v-for="conversation in pinnedConversations"
+        @click="navigateToConversation(conversation)"
+        style="cursor: pointer"
+      >
+        <j-flex a="center" gap="200">
+          <j-text nomargin>{{ conversation.name }}</j-text>
+        </j-flex>
+      </div>
+    </j-flex>
+  </j-box>
+</template>
+
+<script setup lang="ts">
+import { useCommunityService } from "@/composables/useCommunityService";
+import { defineOptions } from "vue";
+import { useRoute, useRouter } from "vue-router";
+
+defineOptions({ name: "RecentConversations" });
+
+const route = useRoute();
+const router = useRouter();
+const { pinnedConversations, pinnedConversationsLoading } = useCommunityService();
+
+function navigateToConversation(conversation: any) {
+  router.push({
+    name: "view",
+    params: { communityId: route.params.communityId, channelId: conversation.channelId, viewId: "conversation" },
+  });
+}
+</script>
+
+<style lang="scss" scoped></style>
