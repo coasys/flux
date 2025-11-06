5e6643e0dbdb7118139bb1a37f1da93ff0947b14
HexaField <10372036+HexaField@users.noreply.github.com>
2025-11-06 13:16:12 +1100

    Refactor React components and configuration for improved structure and clarity

--- packages/create/templates/react/src/main.ts
+++ packages/create/templates/react/src/main.ts
@@ -1,15 +1,10 @@
-import "preact/debug";
-
-import "@coasys/flux-ui/dist/main.d.ts";
-import { toCustomElement } from "@coasys/ad4m-react-hooks";
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
 import Plugin from "./Plugin";
+import { reactToWebComponent } from "@coasys/ad4m-react-hooks";
 
-const CustomElement = toCustomElement(
-  Plugin,
-  ["perspective", "agent", "source"],
-  {
-    shadow: false,
-  }
-);
+const CustomElement = reactToWebComponent(Plugin, React, ReactDOM, {
+  observedProps: ["perspective", "agent", "source"],
+});
 
 export default CustomElement;
