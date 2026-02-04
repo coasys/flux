f156908d21d5ac5ee0eb08317ee7a655091bd536
jhweir <james@weco.io>
2026-02-04 17:40:35 +0000

    Old JoinCommunityFromLinkModal & redundant comments removed

--- app/src/stores/communityServiceStore.ts
+++ app/src/stores/communityServiceStore.ts
@@ -8,14 +8,10 @@ export const useCommunityServiceStore = defineStore(
     const communityServices = ref<Record<string, CommunityService>>({});
 
     function addCommunityService(communityUrl: string, service: CommunityService) {
-      console.log('Adding community service for:', communityUrl);
       communityServices.value[communityUrl] = service;
     }
 
     function getCommunityService(communityUrl: string): CommunityService | undefined {
-      // console.log('999 Getting community service for:', communityUrl);
-      // console.log('999 Available community services:', communityServices.value);
-      // console.log('999 Community service found:', communityServices.value[communityUrl]);
       return communityServices.value[communityUrl];
     }
 
