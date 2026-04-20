8190798f81f1fab535ee4275b77809af676e401e
Nicolas Luck <nicolas@lucksus.eu>
2026-04-20 15:52:11 +0200

    Update hardwired languages

--- packages/constants/src/languages.ts
+++ packages/constants/src/languages.ts
@@ -1,3 +1,3 @@
-export const FILE_STORAGE_LANGUAGE = 'QmzSYwdjqeP9D13Sfmyc5HcabM9jL3DtPyhadnF6dQXu4FjVSbQ';
+export const FILE_STORAGE_LANGUAGE = 'QmzSYwdZwngzefDWzbBzKuY9xP4jnrNoAYDQyvNQF5o1zbUDLa9';
 
-export const EMBEDDING_VECTOR_LANGUAGE = 'QmzSYwdbqjGGbYbWJvdKA4WnuFwmMx3AsTfgg7EwbeNUGyE555c';
+export const EMBEDDING_VECTOR_LANGUAGE = 'QmzSYwdePqDwYwVj2ChhMnyD5Z3Zxue916dUFrptLhhQv3AqUDR';
